31a3aa3d4cbba6ce6f4051213e673fa2199b5e81
Connor Feeley <cfeeley@cfeeley.org>
2022-08-22 20:02:26 -0400

    feat(hm|zsh): port imperative zgen config

--- home/profiles/shells/zsh/default.nix
+++ home/profiles/shells/zsh/default.nix
@@ -15,8 +15,6 @@ in {
     zsh
   ];
 
-  programs.starship.enableZshIntegration = true;
-
   programs.zsh = {
     inherit
       envExtra
@@ -35,7 +33,92 @@ in {
     history.extended = true;
     history.ignoreDups = true;
 
-    # plugins = with pkgs; [];
+    plugins = with pkgs; [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "junegunn";
+          repo = "fzf";
+          rev = "0.32.1";
+          sha256 = "194xz7jrjsl9i88gy9kr90sw9aif9iskwdnhbq85l48h86hlrm42";
+        };
+      }
+      {
+        name = "zsh-vi-mode";
+        src = pkgs.fetchFromGitHub {
+          owner = "jeffreytse";
+          repo = "zsh-vi-mode";
+          rev = "v0.8.5";
+          sha256 = "1wgkqy89qp1kkg64brm42rx3apsfhmpada57vci0lwib3lg2mrhh";
+        };
+      }
+      {
+        name = "fast-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zdharma-continuum";
+          repo = "fast-syntax-highlighting";
+          rev = "v1.55";
+          sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
+        };
+      }
+      {
+        name = "zsh-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-completions";
+          rev = "0.34.0";
+          sha256 = "0jjgvzj3v31yibjmq50s80s3sqi4d91yin45pvn3fpnihcrinam9";
+        };
+      }
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "v1.0.2";
+          sha256 = "0y8va5kc2ram38hbk2cibkk64ffrabfv1sh4xm7pjspsba9n5p1y";
+        };
+      }
+      {
+        name = "zsh-autopair";
+        src = pkgs.fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "v1.0";
+          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
+        };
+      }
+      {
+        name = "zsh-z";
+        src = pkgs.fetchFromGitHub {
+          owner = "agkozak";
+          repo = "zsh-z";
+          rev = "aaafebcd97424c570ee247e2aeb3da30444299cd";
+          sha256 = "147rwiqn5xs0vx7pkqvl1480s7fv7f5879cq6k42pn74jawzhspm";
+        };
+      }
+      #####################
+      # {
+      #   name = "";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "";
+      #     repo = "";
+      #     rev = "";
+      #     sha256 = "";
+      #   };
+      # }
+      # {
+      #   name = "enhancd";
+      #   file = "init.sh";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "b4b4r07";
+      #     repo = "enhancd";
+      #     rev = "v2.2.1";
+      #     sha256 = "0iqa9j09fwm6nj5rpip87x3hnvbbz9w9ajgm6wkrd5fls8fn8i5g";
+      #   };
+      # }
+    ];
 
     initExtraFirst = ''
       # if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
@@ -51,6 +134,7 @@ in {
       source $DOTFIELD_DIR/lib/color.sh
       source $DOTFIELD_DIR/config/zsh/functions.zsh
       source $DOTFIELD_DIR/config/zsh/options.zsh
+      source $DOTFIELD_DIR/config/emacs/vterm.zsh
     '';
 
     sessionVariables = {
