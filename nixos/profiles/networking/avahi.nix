90f5120587227b71672e0874d477edb25372a89e
Connor Feeley <cfeeley@cfeeley.org>
2022-08-22 20:08:51 -0400

    feat(nixos|avahi): init module

---
+++ nixos/profiles/networking/avahi.nix
@@ -0,0 +1,20 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+
+{
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+}
