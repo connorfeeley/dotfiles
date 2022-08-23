f6e965a40fc21c922d295b382c2f4cc0ccf709ee
Connor Feeley <cfeeley@cfeeley.org>
2022-08-22 20:03:25 -0400

    fix(hm|shells): disable atuin

--- home/profiles/shells/common.nix
+++ home/profiles/shells/common.nix
@@ -37,14 +37,4 @@ in {
       BASH_COMPLETION_USER_FILE = "${config.xdg.dataHome}/bash/completion";
     };
   };
-
-  # Magical shell history
-  programs.atuin.enable = true;
-  programs.atuin.settings = {
-    auto_sync = true;
-    sync_frequency = "10m";
-    sync_address = "https://api.atuin.sh";
-    search_mode = "fuzzy"; # 'prefix' | 'fulltext' | 'fuzzy'
-    filter_mode = "global"; # 'global' | 'host' | 'session' | 'directory'
-  };
 }
