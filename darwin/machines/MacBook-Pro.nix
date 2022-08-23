50e4e0219655a3e16eab950e213a33fed0fae40b
Connor Feeley <cfeeley@cfeeley.org>
2022-08-23 00:35:49 -0400

    feat(hm|darwin): symlink HM apps to ~/Applications

--- darwin/machines/MacBook-Pro.nix
+++ darwin/machines/MacBook-Pro.nix
@@ -1,24 +1,57 @@
-{
-  config,
-  lib,
-  pkgs,
-  profiles,
-  suites,
-  inputs,
-  primaryUser,
-  collective,
-  ...
-}: let
+{ config
+, lib
+, pkgs
+, profiles
+, suites
+, inputs
+, primaryUser
+, collective
+, ...
+}:
+let
   inherit (collective) peers;
   inherit (config.networking) hostName;
-in {
+in
+{
   ### === users ================================================================
 
   dotfield.guardian.enable = true;
   dotfield.guardian.username = "cfeeley";
 
   home-manager.users = {
-    cfeeley = hmArgs: {imports = with hmArgs.roles; workstation;};
+    cfeeley = hmArgs: {
+      imports = with hmArgs.roles; workstation;
+
+      home.username = hmArgs.lib.mkForce "cfeeley";
+      home.homeDirectory = hmArgs.lib.mkForce "/Users/cfeeley";
+
+      # Symlink Nix applications to '~/Applications/Home Manager Apps'
+      # https://github.com/nix-community/home-manager/issues/1341#issuecomment-1190875080
+      # https://github.com/nix-community/home-manager/pull/3139
+      # FIXME: ~/Applications must be created manually first
+      home.activation = lib.mkIf pkgs.stdenv.isDarwin {
+        copyApplications =
+          let
+            apps = pkgs.buildEnv {
+              name = "home-manager-applications";
+              paths = hmArgs.config.home.packages;
+              pathsToLink = "/Applications";
+            };
+          in
+          hmArgs.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            baseDir="$HOME/Applications/Home Manager Apps"
+            if [ -d "$baseDir" ]; then
+              rm -rf "$baseDir"
+            fi
+            mkdir -p "$baseDir"
+            for appFile in ${apps}/Applications/*; do
+              target="$baseDir/$(basename "$appFile")"
+              $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+              $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+            done
+          '';
+      };
+    };
   };
 
   networking.hostName = "MacBook-Pro";
