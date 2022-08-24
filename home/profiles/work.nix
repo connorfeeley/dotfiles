374da10fe3eb937428b38e417bf83ffe91767ee2
Connor Feeley <cfeeley@cfeeley.org>
2022-08-24 14:32:48 -0400

    fix(darwin): only enable remmina on linux

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -4,12 +4,15 @@
   pkgs,
   ...
 }:
-{
+let
+  inherit (pkgs.stdenv.hostPlatform) isLinux system;
+in {
   home.packages = with pkgs; [
     pwrbar   # control Kasa WiFi powerbar
     picocom  # minicom without the cruft
-    remmina  # RDP/VNC client
 
     dtc      # device tree compiler
-  ];
+  ] ++ lib.optionals isLinux (with pkgs; [
+    remmina  # RDP/VNC client
+  ]);
 }
