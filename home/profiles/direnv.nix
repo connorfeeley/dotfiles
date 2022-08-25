4b4e93ea5b9963d5914223400f866f691c214486
Connor Feeley <cfeeley@cfeeley.org>
2022-08-24 22:38:12 -0400

    feat: configure direnv

--- home/profiles/direnv.nix
+++ home/profiles/direnv.nix
@@ -7,5 +7,8 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+
+    enableBashIntegration = true;
   };
+  xdg.configFile."direnv/direnvrc".source = "${pkgs.dotfield-config}/direnv/direnvrc";
 }
