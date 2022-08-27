aa71446816bf8ad9d58f734b522188045f1d8d6f
Connor Feeley <cfeeley@cfeeley.org>
2022-08-27 15:35:48 -0400

    feat(sshd): configure for gpg-agent forwarding

--- profiles/networking/ssh-host.nix
+++ profiles/networking/ssh-host.nix
@@ -9,4 +9,8 @@
   services.openssh.permitRootLogin = "prohibit-password";
   # FIXME: too open!!! set per-host explicitly.
   users.users.root.openssh.authorizedKeys.keys = primaryUser.authorizedKeys;
+
+  services.openssh.extraConfig = ''
+    StreamLocalBindUnlink yes
+  '';
 }
