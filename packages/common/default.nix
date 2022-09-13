1b628a054837b8923bc2219234006c6995dda8df
Connor Feeley <cfeeley@cfeeley.org>
2022-09-12 22:27:11 -0400

    feat(svlangserver): add wrapper to easily find on PATH

--- packages/common/default.nix
+++ packages/common/default.nix
@@ -1,12 +1,20 @@
 final: prev: {
   kitty-helpers = final.lib.recurseIntoAttrs (final.callPackage ./kitty-helpers.nix {});
 
-  svlangserver = (final.callPackage ./svlangserver { }).package.override {
+  svlangserver-unwrapped = (final.callPackage ./svlangserver { }).package.override {
     src = builtins.fetchGit {
       url = "https://github.com/imc-trading/svlangserver.git";
       ref = "master";
       rev = "7f53c7f3394447bdd06de9566cd7240aa6cf0c8e";
     };
+    nodejs = final.nodejs-14_x;
+  };
+
+  svlangserver =
+    final.writeShellApplication {
+    name = "svlangserver";
+    text = with final; "exec ${svlangserver-unwrapped.nodejs}/bin/node ${svlangserver-unwrapped}/lib/node_modules/@imc-trading/svlangserver/bin/main.js";
+    runtimeInputs = with final; [ nodejs-14_x svlangserver-unwrapped ];
   };
 
   svls = final.callPackage ./svls.nix { };
