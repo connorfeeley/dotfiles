1cc3948ccddf740a6fbb8f9cfc4d31d1c351f214
Connor Feeley <cfeeley@cfeeley.org>
2022-09-13 22:12:25 -0400

    feat(hm|security): add a collection of sec. tools

--- home/profiles/security-tools.nix
+++ home/profiles/security-tools.nix
@@ -8,9 +8,41 @@
   home.packages = with pkgs; [
     ## === Reverse Engineering ===
     (binwalk.override { visualizationSupport = true; })
+    radare2
+    radare2-cutter
 
-    ## === Local Development ===
+    ## === Crypto-Stego ===
+    ccrypt
 
-    act # Run GitHub Actions locally
+    ## === Forensics ===
+    chkrootkit
+    ddrescue
+    exiv2
+    pdf-parser
+
+    ## === Networking ===
+    arping
+    nmap
+    ncrack
+    aircrack-ng
+    wpscan
+    tcpreplay
+    tcpdump
+    wireshark
+    proxychains
+    udptunnel
+    macchanger
+    mitmproxy
+    spike # Network protocol fuzzer
+    sslsplit
+
+    ## === Information Gathering ===
+    theharvester
+
+    ## === Passwords ===
+    hashcat
+    hashcat-utils
+    thc-hydra
+    john
   ];
 }
