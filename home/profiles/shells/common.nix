62b7df8e102da4d56942f38f4eecee655e354c45
Connor Feeley <cfeeley@cfeeley.org>
2022-09-16 15:37:04 -0400

    Revert "feat(shell|hm): add git-format script"
    This reverts commit 8e70d7b3f55f9e1bd774c1d53b23d2df167e44f6.

--- home/profiles/shells/common.nix
+++ home/profiles/shells/common.nix
@@ -55,27 +55,12 @@ in
           nix --log-format internal-json "$@" |& ${pkgs.nix-json-progress}/bin/nix-json-progress
         '';
       };
-      git-format =
-        let
-          src = pkgs.fetchFromGitHub {
-            owner = "kimgr";
-            repo = "git-format";
-            rev = "8c7e32d74fdd8a3498390846f22f1baa953bbeb2";
-            sha256 = "sha256-bJ+4Mz5JfMm8V4UWhkkJM/xBFTnfpbiZLOwkAYAabjg=";
-          };
-        in
-        pkgs.writeShellApplication {
-          name = "git-format";
-          runtimeInputs = with pkgs; [ python3 ];
-          text = "${src}/git-format";
-        };
     in
     [
       md
       rsc
       stderred-wrapper # Highlight stderr in red
       fnix
-      git-format
     ];
 
   programs.bash = {
