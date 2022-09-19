041f139a6f390ccda3304d825c3ee0bb82f0bb73
Connor Feeley <cfeeley@cfeeley.org>
2022-09-19 13:36:28 -0400

    fix(darwin): make 'pwrbar' linux-only

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -8,12 +8,13 @@ let
   inherit (pkgs.stdenv.hostPlatform) isLinux system;
 in {
   home.packages = with pkgs; [
-    pwrbar   # control Kasa WiFi powerbar
     picocom  # minicom without the cruft
-
     dtc      # device tree compiler
   ] ++ lib.optionals isLinux (with pkgs; [
     remmina  # RDP/VNC client
     freerdp
+
+    # FIXME(darwin): broken
+    pwrbar   # control Kasa WiFi powerbar
   ]);
 }
