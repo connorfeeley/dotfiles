987678ad4a8cdf8f4467a842d695264d4b03f4cc
Connor Feeley <cfeeley@cfeeley.org>
2022-10-01 00:00:00 -0400

    Revert "tweak(ssh|hm): drop ServerAliveInterval to 4"
    This reverts commit 96be85936bb82dfb51c68b4b339c2b10d7727d0f.

--- home/profiles/ssh.nix
+++ home/profiles/ssh.nix
@@ -56,7 +56,7 @@ in {
         addressFamily = "inet";
         forwardX11 = false;
         forwardX11Trusted = false;
-        serverAliveInterval = 4;
+        serverAliveInterval = 300;
         serverAliveCountMax = 2;
 
         extraOptions = {
