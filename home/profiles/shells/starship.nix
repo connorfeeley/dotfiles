1ae7b55adff980e2a90c4b637aaea18282354187
Connor Feeley <cfeeley@cfeeley.org>
2022-10-03 00:00:00 -0400

    feat(starship|hm): simplify prompt to 1-line

--- home/profiles/shells/starship.nix
+++ home/profiles/shells/starship.nix
@@ -130,12 +130,8 @@ in
       format = mkPrompt "$" [
         "username"
         "directory"
-        "git_branch"
         "git_state"
-        "git_status"
         "cmd_duration"
-        "line_break"
-        "python"
         "character"
       ];
 
