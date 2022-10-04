9a43febec00469c4e61dd8466116c79a38e7bfaa
Connor Feeley <cfeeley@cfeeley.org>
2022-10-04 00:00:00 -0400

    reenable nativeComp

--- packages/common/emacs-macport/generic.nix
+++ packages/common/emacs-macport/generic.nix
@@ -38,7 +38,7 @@
 , withWebP ? false
 , srcRepo ? true, autoreconfHook ? null, texinfo ? null
 , siteStart ? ./site-start.el
-, nativeComp ? false
+, nativeComp ? true
 , withAthena ? false
 , withToolkitScrollBars ? true
 , withPgtk ? false, gtk3 ? null
