370b7bfae6023dc1d397d3d4e696a5586257b09e
Connor Feeley <cfeeley@cfeeley.org>
2022-10-25 00:00:00 -0400

    feat(darwin:safari): add reddit extension

--- darwin/profiles/gui/safari.nix
+++ darwin/profiles/gui/safari.nix
@@ -7,5 +7,6 @@
   homebrew.masApps = {
     "Vimari" = 1480933944; # Safari port of Vimium
     "AdGuard for Safari" = 1440147259; # Not quite uBlock origin
+    "Tweaks for Reddit" = 1524828965; # RES-lite
   };
 }
