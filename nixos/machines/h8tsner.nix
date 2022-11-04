6b64a15224186838041305cbcd3a7d90df6bca8e
Connor Feeley <cfeeley@cfeeley.org>
2022-11-04 00:00:00 -0400

    feat(nixos:h8tsner): flesh out configuration a bit

--- nixos/machines/h8tsner.nix
+++ nixos/machines/h8tsner.nix
@@ -9,9 +9,15 @@
 , pkgs
 , profiles
 , suites
+, inputs
+, primaryUser
+, collective
 , ...
 }:
 let
+  inherit (collective) peers;
+  inherit (config.networking) hostName;
+
   # Bootstrap sets up:
   # - enabling systemd-boot
   # - a 'nixos' NixOS and HM user
@@ -31,11 +37,63 @@ in
       Name = eth0
       [Network]
       # Add your own assigned ipv6 subnet here here!
-      Address = 2a01:4f9:ffff::1/64
+      Address = 2a01:4ff:f0:4cff::/64
       Gateway = fe80::1
       # optionally you can do the same for ipv4 and disable DHCP (networking.dhcpcd.enable = false;)
-      # Address =  144.x.x.x/26
-      # Gateway = 144.x.x.1
+      Address =  5.161.105.71/26
+      Gateway = 5.161.105.1
     '';
+  };
+  networking.dhcpcd.enable = false;
+
+  # Mount /tmp as tmpfs
+  boot.tmpOnTmpfs = true;
+
+  ### === timezone ============================================================
+
+  time = {
+    timeZone = "America/Toronto";
+    hardwareClockInLocalTime = true;
+  };
+  environment.sessionVariables.TZ = "${config.time.timeZone}";
+  location = {
+    provider = "manual";
+    latitude = 43.70011;
+    longitude = -79.4163;
+  };
+
+  ### === users ================================================================
+
+  dotfield.guardian.enable = true;
+  dotfield.guardian.username = "cfeeley";
+
+  users.mutableUsers = false;
+  users.users.root.hashedPassword = "$6$V/uLpKYBvGk/Eqs7$IMguTPDVu5v1B9QBkPcIi/7g17DPfE6LcSc48io8RKHUjJDOLTJob0qYEaiUCAS5AChK.YOoJrpP5Bx38XIDB0";
+  users.users.cfeeley = {
+    uid = 1000;
+    isNormalUser = true;
+    initialHashedPassword = "$6$V/uLpKYBvGk/Eqs7$IMguTPDVu5v1B9QBkPcIi/7g17DPfE6LcSc48io8RKHUjJDOLTJob0qYEaiUCAS5AChK.YOoJrpP5Bx38XIDB0";
+    hashedPassword = "$6$V/uLpKYBvGk/Eqs7$IMguTPDVu5v1B9QBkPcIi/7g17DPfE6LcSc48io8RKHUjJDOLTJob0qYEaiUCAS5AChK.YOoJrpP5Bx38XIDB0";
+    openssh.authorizedKeys.keys = primaryUser.authorizedKeys;
+    extraGroups =
+      [
+        "wheel"
+        "video"
+        "audio"
+        "networkmanager"
+        "dialout"
+        "cfeeley"
+        "secrets"
+        "wireshark"
+      ]
+      ++ (lib.optional config.services.mysql.enable "mysql")
+      ++ (lib.optional config.virtualisation.docker.enable "docker")
+      ++ (lib.optional config.virtualisation.podman.enable "podman")
+    ;
+    shell = pkgs.zsh;
+  };
+
+  home-manager.users = {
+    cfeeley = hmArgs: { imports = with hmArgs.roles; workstation ++ linux; };
   };
 }
--- nixos/machines/workstation/default.nix
+++ nixos/machines/workstation/default.nix
@@ -139,7 +139,8 @@ in
       ++ (lib.optional config.virtualisation.docker.enable "docker")
       ++ (lib.optional config.virtualisation.podman.enable "podman")
       ++ (lib.optional config.virtualisation.libvirtd.enable "libvirtd")
-      ++ (lib.optional config.virtualisation.virtualbox.host.enable "vboxusers");
+      ++ (lib.optional config.virtualisation.virtualbox.host.enable "vboxusers")
+    ;
     shell = pkgs.zsh;
   };
 
