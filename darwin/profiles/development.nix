65e40e3f3206e6b25db25c9367a9147490929158
Connor Feeley <cfeeley@cfeeley.org>
2022-11-05 00:00:00 -0400

    feat(darwin:dev): add aircrack-ng brew

--- darwin/profiles/development.nix
+++ darwin/profiles/development.nix
@@ -13,4 +13,9 @@
     darwin.DarwinTools
     darwin.ios-deploy
   ];
+
+  # I'm not *quite* sure this belongs here.
+  homebrew.brews = [
+    "aircrack-ng" # History lesson: WEP was deprecated in 2004. No, I didn't drop a decade somewhere.
+  ];
 }
