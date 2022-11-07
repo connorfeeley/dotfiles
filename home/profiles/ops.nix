857662eb5f93d9edf5d41cee11522fb681f3edde
Connor Feeley <cfeeley@cfeeley.org>
2022-11-07 00:00:00 -0500

    feat(hm:ops): add hetzner cloud CLI

--- home/profiles/ops.nix
+++ home/profiles/ops.nix
@@ -4,5 +4,8 @@
 , pkgs
 , ...
 }: {
-  home.packages = with pkgs; [ nixops ];
+  home.packages = with pkgs; [
+    nixops # <- Use Nix to operate on Nix OS operations.
+    hcloud # <- CLI for Hetzner Cloud.
+  ];
 }
