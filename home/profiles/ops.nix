20e2a4f14ce2361a57a6128403f9666c7b5d18f0
Connor Feeley <cfeeley@cfeeley.org>
2022-11-08 00:00:00 -0500

    feat(hm:ops): add hetzner cloud tool

--- home/profiles/ops.nix
+++ home/profiles/ops.nix
@@ -6,6 +6,10 @@
 }: {
   home.packages = with pkgs; [
     nixops # <- Use Nix to operate on Nix OS operations.
+
+    # Hetzner Cloud management tool
+    # Configure the hcloud program to use your token:
+    # - hcloud context create my-project
     hcloud # <- CLI for Hetzner Cloud.
   ];
 }
