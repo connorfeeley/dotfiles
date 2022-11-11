fd5d5305aab3d83b224c69815f639dec8f8754ba
Connor Feeley <cfeeley@cfeeley.org>
2022-11-11 00:00:00 -0500

    fix(nixos:builder): correct MBP builder public key

--- nixos/profiles/builder.nix
+++ nixos/profiles/builder.nix
@@ -34,9 +34,12 @@ lib.mkMerge [
       buildMachines = [
         {
           hostName = "MacBook-Pro";
-          sshUser = config.dotfield.guardian.username;
           protocol = "ssh";
           systems = [ "aarch64-darwin" "x86_64-darwin" ];
+          # Base64-encoded ed25519 public host key of builder:
+          #   base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtWcnlic0pyQU5yTytGc2xTNU1GcnBNVHRjMkVCZ1hyaUVvRjRzcnFRcnggCg==";
+          sshUser = config.dotfield.guardian.username;
           # "8 laptop cores times 1" seems like a reasonable relative speed factor.
           speedFactor = 8;
           supportedFeatures = [ "big-parallel" "kvm" "nixos-test" "benchmark" ];
