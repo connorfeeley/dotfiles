1de4e17688ad5b7fdf159f7470c08e0e2fb3777e
Connor Feeley <cfeeley@cfeeley.org>
2022-11-11 00:00:00 -0500

    wip

--- nixos/machines/rosy/default.nix
+++ nixos/machines/rosy/default.nix
@@ -35,6 +35,11 @@ let
 in
 # https://github.com/nix-community/nixos-install-scripts/blob/master/hosters/hetzner-dedicated/hetzner-dedicated-wipe-and-install-nixos.sh
 {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
   ### === networking ================================================================
 
   # Via: https://nixos.wiki/wiki/Install_NixOS_on_Hetzner_Online
@@ -117,4 +122,28 @@ in
   ### === misc ================================================================
 
   system.stateVersion = "22.05";
+
+  ### === rosetta ================================================================
+  # https://xyno.space/post/nixos-utm-rosetta
+
+  boot.initrd.availableKernelModules = [ "virtiofs" ];
+  fileSystems."/run/rosetta" = {
+    device = "rosetta";
+    fsType = "virtiofs";
+  };
+  fileSystems."/media/share" = {
+    device = "share";
+    fsType = "virtiofs";
+  };
+  nix.settings.extra-platforms = [ "x86_64-linux" ];
+  nix.settings.extra-sandbox-paths = [ "/run/rosetta" "/run/binfmt" ];
+  boot.binfmt.registrations."rosetta" = {
+    # based on https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta#3978495
+    interpreter = "/run/rosetta/rosetta";
+    fixBinary = true;
+    wrapInterpreterInShell = false;
+    matchCredentials = true;
+    magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+    mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+  };
 }
