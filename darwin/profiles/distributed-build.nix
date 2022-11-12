1c7e876c1536bc6687ddfe40189ab086fee96074
Connor Feeley <cfeeley@cfeeley.org>
2022-11-12 00:00:00 -0500

    fix(darwin): only use rosy for remote builds

--- darwin/profiles/distributed-build.nix
+++ darwin/profiles/distributed-build.nix
@@ -19,38 +19,38 @@ lib.mkMerge [
       distributedBuilds = true;
       buildMachines = [
         {
-          hostName = "workstation";
-          maxJobs = 8;
-          systems = [ "x86_64-linux" "aarch64-linux" ];
-          # Base64-encoded ed25519 public host key
-          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
-          sshUser = config.dotfield.guardian.username;
-          # 12 desktop cores, times two (versus times 1 for a laptop)" seems like a reasonable relative speed factor.
-          speedFactor = 12 * 2;
-          supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        }
-        {
           hostName = "rosy";
           maxJobs = 8;
           systems = [ "aarch64-linux" "x86_64-linux" ];
           # Base64-encoded ed25519 public host key
           publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtsM2ZZMXAvQnNjUmlyQXpweFltdDdSanFneWQvT0RCS2xnR2hjNUd6d2Egcm9vdEBuaXhvcwo=";
-          sshUser = "root";
+          sshUser = config.dotfield.guardian.username;
           # "8 M1 cores, times one (versus times 2 for a desktop)" seems like a reasonable relative speed factor.
           speedFactor = 8 * 1;
           supportedFeatures = [ "nixos-test" "benchmark" "kvm" ];
         }
-        {
-          # https://github.com/LnL7/nix-docker
-          # docker run --restart always --name nix-docker -d -p 3022:22 lnl7/nix:ssh
-          hostName = "nix-docker-build-slave";
-          systems = [ "x86_64-linux" ];
-          # Base64-encoded ed25519 public host key
-          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
-          # "8 M1 cores, times one (versus times 2 for a desktop)" seems like a reasonable relative speed factor.
-          speedFactor = 8 * 1;
-          supportedFeatures = [ "nixos-test" "benchmark" ];
-        }
+        # {
+        #   hostName = "workstation";
+        #   maxJobs = 8;
+        #   systems = [ "x86_64-linux" "aarch64-linux" ];
+        #   # Base64-encoded ed25519 public host key
+        #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
+        #   sshUser = config.dotfield.guardian.username;
+        #   # 12 desktop cores, times two (versus times 1 for a laptop)" seems like a reasonable relative speed factor.
+        #   speedFactor = 12 * 2;
+        #   supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        # }
+        # {
+        #   # https://github.com/LnL7/nix-docker
+        #   # docker run --restart always --name nix-docker -d -p 3022:22 lnl7/nix:ssh
+        #   hostName = "nix-docker-build-slave";
+        #   systems = [ "x86_64-linux" ];
+        #   # Base64-encoded ed25519 public host key
+        #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
+        #   # "8 M1 cores, times one (versus times 2 for a desktop)" seems like a reasonable relative speed factor.
+        #   speedFactor = 8 * 1;
+        #   supportedFeatures = [ "nixos-test" "benchmark" ];
+        # }
       ];
     };
   }
