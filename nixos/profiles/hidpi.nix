56a4c58ccfccfcecd7359098592a85ec7c8666c5
Connor Feeley <cfeeley@cfeeley.org>
2022-11-13 00:00:00 -0500

    imp(nixos:hidpi): set console mode and font

--- nixos/profiles/hidpi.nix
+++ nixos/profiles/hidpi.nix
@@ -3,5 +3,14 @@
 , pkgs
 , ...
 }: {
-  hardware.video.hidpi.enable = true;
+  # hardware.video.hidpi.enable = true;
+
+  # Larger console font
+  console.font = lib.mkDefault "latarcyrheb-sun32";
+
+  # Also use large console font in initrd
+  console.earlySetup = true;
+
+  # Try use maximum resolution in systemd-boot
+  boot.loader.systemd-boot.consoleMode = lib.mkDefault "max";
 }
