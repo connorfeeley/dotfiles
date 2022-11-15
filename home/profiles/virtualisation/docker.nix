825dbb741407f30a49f722ac60be43ebbbd16f6a
Connor Feeley <cfeeley@cfeeley.org>
2022-11-15 00:00:00 -0500

    feat(hm:docker): add CLI plugins

--- home/profiles/virtualisation/docker.nix
+++ home/profiles/virtualisation/docker.nix
@@ -3,5 +3,18 @@
 , pkgs
 , ...
 }: {
-  home.packages = with pkgs; [ docker docker-compose docker-buildx ];
+  /*
+  Setup docker CLI plugins:
+    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    ln -s $(which docker-compose) $DOCKER_CONFIG/cli-plugins/docker-compose
+    ln -s $(which docker-buildx) $DOCKER_CONFIG/cli-plugins/docker-buildx
+  */
+  home.packages = with pkgs; [
+    docker
+    docker-compose
+    docker-buildx
+    docker-credential-helpers
+    buildkit
+  ];
 }
