35c8ea8be81660cd85674ae229dedb35a7cfab18
Connor Feeley <cfeeley@cfeeley.org>
2022-11-15 00:00:00 -0500

    feat(nixos:wkstn): enable webdav

--- nixos/machines/workstation/default.nix
+++ nixos/machines/workstation/default.nix
@@ -126,7 +126,7 @@ in
     };
   };
 
-  ### === NFS share ============================================================
+  ### === Shares ============================================================
   fileSystems."/mnt/export/cfeeley" = {
     device = "/home/cfeeley";
     options = [ "bind" ];
@@ -137,6 +137,8 @@ in
     /mnt/export         100.66.73.0/24(rw,fsid=0,no_subtree_check,all_squash,anonuid=0,anongid=100)
     /mnt/export/cfeeley 100.66.73.0/24(rw,nohide,insecure,no_subtree_check,all_squash,anonuid=0,anongid=100)
   '';
+
+  services.webdav.enable = true;
 
   ### === users ================================================================
 
