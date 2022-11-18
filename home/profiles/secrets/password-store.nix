d9a612dc158bb8aca2ba172d8ee71e2323a5a62a
Connor Feeley <cfeeley@cfeeley.org>
2022-11-18 00:00:00 -0500

    feat(hm:secrets): switch pass -> gopass

--- home/profiles/secrets/password-store.nix
+++ home/profiles/secrets/password-store.nix
@@ -6,17 +6,19 @@
 let
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
   inherit (config.lib.dotfield.whoami) pgpPublicKey;
+
+  passWithExtensions = pkgs.pass.withExtensions (exts:
+    with exts; [
+      pass-import # https://github.com/roddhjav/pass-import
+      pass-otp # https://github.com/tadfisher/pass-otp
+      pass-update # https://github.com/roddhjav/pass-update
+    ]);
 in
 lib.mkMerge [
   {
     programs.password-store = lib.mkIf config.programs.gpg.enable {
       enable = true;
-      package = pkgs.pass.withExtensions (exts:
-        with exts; [
-          pass-import # https://github.com/roddhjav/pass-import
-          pass-otp # https://github.com/tadfisher/pass-otp
-          pass-update # https://github.com/roddhjav/pass-update
-        ]);
+      package = pkgs.gopass; # Or passWithExtensions
       settings = {
         PASSWORD_STORE_DIR = "${config.xdg.dataHome}/pass";
         PASSWORD_STORE_KEY = pgpPublicKey;
@@ -24,9 +26,7 @@ lib.mkMerge [
     };
   }
   (lib.mkIf (!isDarwin) {
-    # FIXME: needs further configuration... does not play well with 1password,
-    # for example
-    # services.pass-secret-service.enable = true;
+    services.pass-secret-service.enable = true;
     services.password-store-sync.enable = true;
   })
   (lib.mkIf config.programs.firefox.enable {
