0510b7b3e6a79a8e459bc5a4937c59a6b6f095a5
Connor Feeley <cfeeley@cfeeley.org>
2022-11-18 00:00:00 -0500

    refact(darwin:gui): add empty Nix module options decl

--- darwin/profiles/gui/default.nix
+++ darwin/profiles/gui/default.nix
@@ -2,101 +2,105 @@
 , lib
 , pkgs
 , ...
-}: {
+}:
+{
   imports = [
     ./hammerspoon.nix
     ./yabai.nix
     ./xquartz.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    gtk-mac-integration
-    iterm2 #     <- the king of macOS terminals; not kitty 😾
-    fontconfig # <- appease 'doom doctor'
-    dockutil #   <- command line utility for managing macOS dock items
-    mysides #    <- manages macOS Finder sidebar favorites, tells hilarious jokes
+  options = { };
+  config = {
+    environment.systemPackages = with pkgs; [
+      gtk-mac-integration
+      iterm2 #     <- the king of macOS terminals; not kitty 😾
+      fontconfig # <- appease 'doom doctor'
+      dockutil #   <- command line utility for managing macOS dock items
+      mysides #    <- manages macOS Finder sidebar favorites, tells hilarious jokes
 
-    ferium # CLI program for managing Minecraft mods and modpacks
-  ];
+      ferium # CLI program for managing Minecraft mods and modpacks
+    ];
 
-  environment.variables = {
-    DOOMDIR = "$XDG_CONFIG_HOME/doom";
-    EMACSDIR = "$XDG_CONFIG_HOME/emacs";
-  };
+    environment.variables = {
+      DOOMDIR = "$XDG_CONFIG_HOME/doom";
+      EMACSDIR = "$XDG_CONFIG_HOME/emacs";
+    };
 
-  homebrew.taps = [
-    { name = "FelixKratz/formulae"; }
-    { name = "homebrew/cask"; }
-    { name = "homebrew/cask-versions"; }
-    { name = "railwaycat/emacsmacport"; }
-    { name = "jorgelbg/tap"; }
-  ];
+    homebrew.taps = [
+      { name = "FelixKratz/formulae"; }
+      { name = "homebrew/cask"; }
+      { name = "homebrew/cask-versions"; }
+      { name = "railwaycat/emacsmacport"; }
+      { name = "jorgelbg/tap"; }
+    ];
 
-  homebrew.brews = [
-    # This provides a GUI, despite it not being a cask.
-    # NOTE: now using 'pinentry_mac' package
-    { name = "pinentry-mac"; }
+    homebrew.brews = [
+      # This provides a GUI, despite it not being a cask.
+      # NOTE: now using 'pinentry_mac' package
+      { name = "pinentry-mac"; }
 
-    # Interesting project; however the password is not stored in the
-    # secure enclave, which is a dealbreaker for me 😕.
-    { name = "pinentry-touchid"; }
+      # Interesting project; however the password is not stored in the
+      # secure enclave, which is a dealbreaker for me 😕.
+      { name = "pinentry-touchid"; }
 
-    { name = "freerdp"; }
+      { name = "freerdp"; }
 
-    { name = "alerter"; } # like notify-send but for darwin
-  ];
+      { name = "alerter"; } # like notify-send but for darwin
+    ];
 
-  homebrew.casks = [
-    { name = "emacs-mac"; }
-    { name = "visual-studio-code"; }
+    homebrew.casks = [
+      { name = "emacs-mac"; }
+      { name = "visual-studio-code"; }
 
-    { name = "utm-beta"; } # QEMU GUI with virtualisation support
-    { name = "barrier"; } # Poor man's KVM
-    { name = "swiftbar"; } # Tweak the menu bar
-    { name = "hiddenbar"; } # Hide/toggle menu bar icon visibility
-    { name = "rectangle"; } # Resize windows with keyboard shortcuts and snap areas
-    { name = "raycast"; } # Better search popup
+      { name = "utm-beta"; } # QEMU GUI with virtualisation support
+      { name = "barrier"; } # Poor man's KVM
+      { name = "swiftbar"; } # Tweak the menu bar
+      { name = "hiddenbar"; } # Hide/toggle menu bar icon visibility
+      { name = "rectangle"; } # Resize windows with keyboard shortcuts and snap areas
+      { name = "raycast"; } # Better search popup
 
-    # { name = "docker"; }
+      # { name = "docker"; }
 
-    { name = "karabiner-elements"; }
+      { name = "karabiner-elements"; }
 
-    { name = "firefox"; }
-    { name = "google-chrome"; }
-    { name = "microsoft-edge"; }
+      { name = "firefox"; }
+      { name = "google-chrome"; }
+      { name = "microsoft-edge"; }
 
-    { name = "microsoft-teams"; }
-    { name = "microsoft-outlook"; }
+      { name = "microsoft-teams"; }
+      { name = "microsoft-outlook"; }
 
-    { name = "nordvpn"; }
-    { name = "private-internet-access"; }
+      { name = "nordvpn"; }
+      { name = "private-internet-access"; }
 
-    { name = "vlc"; }
-    { name = "lulu"; }
-    { name = "stats"; }
-    { name = "steam"; }
-    { name = "curseforge"; }
-    { name = "minecraft"; }
-    { name = "feed-the-beast"; }
-    { name = "spotify"; }
-    { name = "deluge"; }
-    { name = "discord"; }
-    { name = "balenaetcher"; }
-  ];
+      { name = "vlc"; }
+      { name = "lulu"; }
+      { name = "stats"; }
+      { name = "steam"; }
+      { name = "curseforge"; }
+      { name = "minecraft"; }
+      { name = "feed-the-beast"; }
+      { name = "spotify"; }
+      { name = "deluge"; }
+      { name = "discord"; }
+      { name = "balenaetcher"; }
+    ];
 
-  homebrew.masApps = {
-    "Xcode" = 497799835;
-    "Keynote" = 409183694;
-    "Numbers" = 409203825;
-    "Pages" = 409201541;
+    homebrew.masApps = {
+      "Xcode" = 497799835;
+      "Keynote" = 409183694;
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
 
-    "Bitwarden" = 1352778147; # MAS version can integrate w/ FF for biometrics
+      "Bitwarden" = 1352778147; # MAS version can integrate w/ FF for biometrics
 
-    "Steam Link" = 1246969117;
+      "Steam Link" = 1246969117;
 
-    # Live broadcasts of the legislature of the province of Ontario, Canada.
-    # Civic engagement is good, mmkay?
-    # FIXME: this is an iPad app, which 'mas' doesn't seem to handle correctly.
-    # "Parlance" = 1520014900;
+      # Live broadcasts of the legislature of the province of Ontario, Canada.
+      # Civic engagement is good, mmkay?
+      # FIXME: this is an iPad app, which 'mas' doesn't seem to handle correctly.
+      # "Parlance" = 1520014900;
+    };
   };
 }
