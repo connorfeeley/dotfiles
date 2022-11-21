c744a213a3e6e6713fcd35f249819281cce406c1
Connor Feeley <cfeeley@cfeeley.org>
2022-11-21 00:00:00 -0500

    refact(hm:ssh): generate match blocks programmatically

--- home/profiles/ssh.nix
+++ home/profiles/ssh.nix
@@ -5,9 +5,32 @@ moduleArgs @ { config
 , ...
 }:
 let
-  inherit (peers.hosts) workstation MacBook-Pro cfeeley-laptop h8tsner rosy;
-  identityFileName = "id_ed25519.pub";
-  identityFile = "~/.ssh/${identityFileName}";
+  inherit (peers.hosts) cfeeley-laptop;
+
+  inherit (pkgs.stdenv) isDarwin;
+
+  gpgAgentForwards = {
+    # if isDarwin: forward GPG-agent extra socket (~/.gnupg/S.gpg-agent.extra) to remote
+    # if isLinux: forward GPG-agent extra socket (/run/user/1000/gnupg/S.gpg-agent.extra) to remote
+    host.address = if isDarwin then "/Users/cfeeley/.gnupg/S.gpg-agent.extra" else "/run/user/1000/gnupg/S.gpg-agent.extra";
+    bind.address = if isDarwin then "/run/user/1000/gnupg/S.gpg-agent.extra" else "/Users/cfeeley/.gnupg/S.gpg-agent.extra";
+  };
+
+  # Creates a matchBlock for ${hostName}.
+  # For ${trusted} hosts, enable trusted X11 forwarding and forward the GPG agent socket.
+  mkMatchBlock = { hostName, trusted ? false }: {
+    port = lib.our.peers.getSshPort hostName;
+
+    # Emacs vterm environment variables
+    extraOptions.SendEnv = "INSIDE_EMACS EMACS_VTERM_PATH";
+
+    # X11 forwarding
+    forwardX11 = lib.mkIf (trusted) true;
+    forwardX11Trusted = lib.mkIf (trusted) true;
+
+    # GPG agent forwarding
+    remoteForwards = lib.mkIf (trusted) [ gpgAgentForwards ];
+  };
 in
 {
   programs.ssh = {
@@ -26,7 +49,6 @@ in
         addressFamily = "inet";
         forwardX11 = false;
         forwardX11Trusted = false;
-
         extraOptions = {
           AddKeysToAgent = "yes";
           ChallengeResponseAuthentication = "no";
@@ -34,7 +56,6 @@ in
           StrictHostKeyChecking = "ask";
           VerifyHostKeyDNS = "yes";
           VisualHostKey = "yes";
-
           PubkeyAcceptedKeyTypes = "+ssh-rsa";
           KexAlgorithms = "+diffie-hellman-group1-sha1";
           HostKeyAlgorithms = "+ssh-rsa";
@@ -42,76 +63,28 @@ in
           # HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa";
           # KexAlgorithms = "curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
           # MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
-
-          # Put John Nagle on a timeout. Har har, get it?
-          # (disable Nagle algorithm)
-          # FIXME: This may not even be an actual configuration option.
-          # NoDelay = "yes" ;
         };
       };
-      "workstation" = {
-        forwardX11 = true;
-        forwardX11Trusted = true;
-        extraOptions.SendEnv = "INSIDE_EMACS EMACS_VTERM_PATH";
-        # GPG agent forwarding
-        remoteForwards = [{
-          bind.address = "/run/user/1000/gnupg/S.gpg-agent";
-          host.address = "/Users/cfeeley/.gnupg/S.gpg-agent.extra";
-        }];
-      };
-      "workstation-luks" = { user = "root"; };
-      "macbook-pro" = {
-        extraOptions.SendEnv = "INSIDE_EMACS EMACS_VTERM_PATH";
-        # GPG agent forwarding
-        remoteForwards = [{
-          bind.address = "/Users/cfeeley/.gnupg/S.gpg-agent";
-          host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
-        }];
-      };
-      # NOTE: manually add entry to root's SSH config (/var/root/.ssh/config) to use as builder
-      "rosy" = {
-        port = lib.our.peers.getSshPort "rosy";
-        forwardX11 = true;
-        forwardX11Trusted = true;
-        extraOptions.SendEnv = "INSIDE_EMACS EMACS_VTERM_PATH";
-        # GPG agent forwarding
-        remoteForwards = [{
-          bind.address = "/run/user/1000/gnupg/S.gpg-agent";
-          host.address = "/Users/cfeeley/.gnupg/S.gpg-agent.extra";
-        }];
-      };
-      "h8tsner" = {
-        hostname = h8tsner.ipv4.address;
-        # GPG agent forwarding
-        remoteForwards = [{
-          bind.address = "/run/user/1000/gnupg/S.gpg-agent";
-          host.address = "/Users/cfeeley/.gnupg/S.gpg-agent.extra";
-        }];
-      };
-      "cfeeley-laptop" = {
-        hostname = cfeeley-laptop.ipv4.address;
-        user = "cfeeley";
-        forwardX11 = true;
-        extraOptions.SendEnv = "INSIDE_EMACS EMACS_VTERM_PATH";
-        # GPG agent forwarding
-        remoteForwards = [{
-          bind.address = "/run/user/1000/gnupg/S.gpg-agent";
-          host.address = "/Users/cfeeley/.gnupg/S.gpg-agent.extra";
-        }];
-      };
 
+      ### Physical
+      "workstation" = mkMatchBlock { hostName = "workstation"; trusted = true; };
+      "workstation-luks" = { user = "root"; extraOptions.RemoteCommand = "cryptsetup-askpass"; };
+      "macbook-pro" = mkMatchBlock { hostName = "MacBook-Pro"; trusted = true; };
+      "cfeeley-laptop" = mkMatchBlock { hostName = "cfeeley-laptop"; trusted = true; } // { hostname = cfeeley-laptop.ipv4.address; };
+
+      ### VMs (local)
+      "rosy" = mkMatchBlock { hostName = "rosy"; }; # NOTE: manually add entry to root's SSH config (/var/root/.ssh/config) to use as builder
+
+      ### VMs (remote)
+      "h8tsner" = mkMatchBlock { hostName = "h8tsner"; };
+
+      ### Other
       "github.com" = {
-        # inherit identityFile;
-        # identitiesOnly = true;
         user = "git";
-        extraOptions = {
-          # "MACs" = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com";
-        };
         extraOptions = {
           ControlMaster = "no";
         };
       };
-
       "10.*.*.*" = {
         extraOptions = {
           KexAlgorithms = "+diffie-hellman-group1-sha1";
@@ -124,4 +97,3 @@ in
 ## References:
 # https://github.com/drduh/config/blob/master/ssh_config
 # https://linux.die.net/man/5/ssh_config
-
