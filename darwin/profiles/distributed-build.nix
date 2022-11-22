93c1808af80e86c73b4102dec78049d0a81277fe
Connor Feeley <cfeeley@cfeeley.org>
2022-11-22 00:00:00 -0500

    feat(darwin): re-enable workstation remote builder

--- darwin/profiles/distributed-build.nix
+++ darwin/profiles/distributed-build.nix
@@ -18,6 +18,20 @@ lib.mkMerge [
     nix = {
       distributedBuilds = true;
       buildMachines = [
+        ### NixOS workstation
+        {
+          hostName = "workstation";
+          maxJobs = 8;
+          systems = [ "x86_64-linux" "aarch64-linux" ];
+          # Base64-encoded ed25519 public host key of builder:
+          #   base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
+          sshUser = config.dotfield.guardian.username;
+          # 12 desktop cores, times two (versus times 1 for a laptop)" seems like a reasonable relative speed factor.
+          speedFactor = 12 * 2;
+          supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        }
+        ### NixOS VM (UTM)
         {
           hostName = "rosy";
           maxJobs = 8;
@@ -33,28 +47,6 @@ lib.mkMerge [
           speedFactor = 8 * 1;
           supportedFeatures = [ "nixos-test" "benchmark" "kvm" ];
         }
-        # {
-        #   hostName = "workstation";
-        #   maxJobs = 8;
-        #   systems = [ "x86_64-linux" "aarch64-linux" ];
-        #   # Base64-encoded ed25519 public host key
-        #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
-        #   sshUser = config.dotfield.guardian.username;
-        #   # 12 desktop cores, times two (versus times 1 for a laptop)" seems like a reasonable relative speed factor.
-        #   speedFactor = 12 * 2;
-        #   supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        # }
-        # {
-        #   # https://github.com/LnL7/nix-docker
-        #   # docker run --restart always --name nix-docker -d -p 3022:22 lnl7/nix:ssh
-        #   hostName = "nix-docker-build-slave";
-        #   systems = [ "x86_64-linux" ];
-        #   # Base64-encoded ed25519 public host key
-        #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUlMK215amtLR0NZSVlrSTE2NXRxL2NwMDRtMGlveDhSTEViNE1TMXdqZXQgcm9vdEBjZmVlbGV5LXdvcmtzdGF0aW9uCg==";
-        #   # "8 M1 cores, times one (versus times 2 for a desktop)" seems like a reasonable relative speed factor.
-        #   speedFactor = 8 * 1;
-        #   supportedFeatures = [ "nixos-test" "benchmark" ];
-        # }
       ];
     };
   }
