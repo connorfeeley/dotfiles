37c1bf4e91060bd3c211778ca339fba9af18c6fd
Connor Feeley <cfeeley@cfeeley.org>
2022-11-22 00:00:00 -0500

    fix(flake): trust devenv cachix public key

--- flake.nix
+++ flake.nix
@@ -544,6 +544,7 @@
   ];
   nixConfig.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
   ];
