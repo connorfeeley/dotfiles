d6d928f740778b4e332e179b2ac5afdca0a4b127
Connor Feeley <cfeeley@cfeeley.org>
2022-12-02 00:00:00 -0500

    fix(nixos): eval errors after update

--- lib/compat/nixos/default.nix
+++ lib/compat/nixos/default.nix
@@ -1,10 +1,8 @@
 { ... }:
 let
-  inherit (default.inputs.nixos-stable) lib;
-
-  host = configs.${hostname} or configs.NixOS;
-  configs = default.nixosConfigurations;
   default = (import ../.).defaultNix;
-  hostname = lib.fileContents /etc/hostname;
+  configs = default.nixosConfigurations;
+  host = configs.${hostname} or configs.workstation;
+  hostname = builtins.getEnv "HOSTNAME";
 in
 host
