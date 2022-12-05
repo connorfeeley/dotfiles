6a2e1f4e41a725e9576c4b33c01c159d282376fa
Connor Feeley <cfeeley@cfeeley.org>
2022-12-05 00:00:00 -0500

    fix(hm:xilinx): update vivado install directory

--- home/profiles/development/xilinx.nix
+++ home/profiles/development/xilinx.nix
@@ -8,7 +8,7 @@
 
   # FIXME: not portable; workstation machine only
   xdg.configFile."xilinx/nix.sh".text = ''
-    INSTALL_DIR=/mnt/ssd/xilinx/vivado
+    INSTALL_DIR=/mnt/zfs/data/Xilinx/Vivado/2021.2
     # The directory in which there's a /bin/ directory for each product, for example:
     # $HOME/downloads/software/xilinx/Vivado/2022.1/bin
     VERSION=2021.2
