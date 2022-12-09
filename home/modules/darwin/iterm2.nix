bc34da4948f0ce6a1ae662b61b5331440de26f9a
Connor Feeley <connor@cfeeley.org>
2022-12-08 19:47:57 -0500

    feat(darwin:hm): export an iterm2 module w/ shell integration

---
+++ home/modules/darwin/iterm2.nix
@@ -0,0 +1,53 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  inherit (pkgs.stdenv) isDarwin;
+  inherit (lib) types mkIf mkEnableOption mkOption;
+
+  shellInit = shell: cfg.package + "/Applications/iTerm2.app/Contents/Resources/iterm2_shell_integration." + shell;
+
+  cfg = config.programs.iterm2;
+in
+{
+  options.programs.iterm2 = {
+    enable = mkEnableOption "Enable the iTerm2 terminal emulator (system-wide).";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.iterm2;
+      description = "The iTerm2 package to use.";
+    };
+
+    enableBashIntegration = mkOption {
+      type = types.bool;
+      default = isDarwin && config.programs.bash.enable;
+      description = "Enable iTerm2 bash integration.";
+    };
+
+    enableZshIntegration = mkOption {
+      type = types.bool;
+      default = isDarwin && config.programs.zsh.enable;
+      description = "Enable iTerm2 zsh integration.";
+    };
+
+    #homeManagerModules.nixvim = import ./wrappers/hm.nix modules;
+    enableFishIntegration = mkOption {
+      type = types.bool;
+      default = isDarwin && config.programs.fish.enable;
+      description = "Enable iTerm2 fish integration.";
+    };
+  };
+
+  config = {
+      home.packages = mkIf cfg.enable [ cfg.package ];
+
+      programs.bash.initExtra = mkIf cfg.enableBashIntegration (shellInit "bash");
+
+      programs.zsh.initExtra = mkIf cfg.enableZshIntegration (shellInit "zsh");
+
+      programs.fish.shellInit = mkIf cfg.enableFishIntegration (shellInit "fish");
+    };
+}
