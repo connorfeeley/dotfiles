346e6f50118145c84424683d03d1ba1b606a76e4
Connor Feeley <cfeeley@cfeeley.org>
2022-12-14 00:00:00 -0500

    feat(nixos): add ddc-monitor-control profile

---
+++ nixos/profiles/desktop/ddc-monitor-control.nix
@@ -0,0 +1,25 @@
+{ lib, config, pkgs, ... }: {
+  boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
+  boot.kernelModules = [ "i2c-dev" "ddcci_backlight" ];
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="i2c-dev", ACTION=="add",\
+      ATTR{name}=="NVIDIA i2c adapter*",\
+      TAG+="ddcci",\
+      TAG+="systemd",\
+      ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+  '';
+
+  systemd.services."ddcci@" = {
+    scriptArgs = "%i";
+    script = ''
+      echo Trying to attach ddcci to $1
+      i=0
+      id=$(echo $1 | cut -d "-" -f 2)
+      if ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id; then
+        echo ddcci 0x37 > /sys/bus/i2c/devices/$1/new_device
+      fi
+    '';
+    serviceConfig.Type = "oneshot";
+  };
+}
