d0da27bfc243b4f06d3ad5ca49e7bf6581c4d6b9
Connor Feeley <connor@cfeeley.org>
2022-12-15 01:19:55 -0500

    fix(darwin): install Amphetamine Enhancer properly

--- darwin/packages/amphetamine-enhancer.nix
+++ darwin/packages/amphetamine-enhancer.nix
@@ -1,47 +1,18 @@
-{ fetchurl, lib, stdenvNoCC, undmg, unzip }:
-let
-  # TODO: break this out into a common function
-  # Source: https://discourse.nixos.org/t/feedback-darwin-installapplication/11324
-  installApplication =
-    { app ? "./", description, homepage, license, maintainers, name, sha256, url, version, sourceRoot ? null, postInstall ? "", ... }:
-    stdenvNoCC.mkDerivation {
-      inherit name version sourceRoot;
-
-      nativeBuildInputs = [ undmg unzip ];
-
-      src = fetchurl {
-        inherit url sha256;
-      };
-
-      phases = [ "unpackPhase" "installPhase" ];
-
-      installPhase = ''
-        set -x
-        mkdir -p "$out/Applications/${app}"
-        mv * "$out/Applications/${app}"
-      '' + postInstall;
-
-      meta = with lib; {
-        description = description;
-        homepage = homepage;
-        license = licenses."${license}";
-        maintainers = forEach maintainers (_x: maintainers."${maintainer}");
-        platforms = platforms.darwin;
-      };
-    };
-in
+{ lib, fetchurl, installApplication }:
 installApplication {
   name = "Amphetamine Enhancer";
-  app = "./Amphetamine Enhancer.app";
-  sourceRoot = "Amphetamine Enhancer.app";
-  version = "1.0";
+  appName = "Amphetamine Enhancer.app";
 
-  url = "https://github.com/x74353/Amphetamine-Enhancer/raw/master/Releases/Current/Amphetamine%20Enhancer.dmg";
-  sha256 = "sha256-qISMBy46rm+J+smftPcbrJy8lrWynMV/fgEjXD45oUw=";
+  src = fetchurl {
+    url = "https://github.com/x74353/Amphetamine-Enhancer/raw/master/Releases/Current/Amphetamine%20Enhancer.dmg";
+    sha256 = "sha256-qISMBy46rm+J+smftPcbrJy8lrWynMV/fgEjXD45oUw=";
+  };
 
-  description = "Add new abilities to the macOS keep-awake utility, Amphetamine.";
-  homepage = "https://github.com/x74353/Amphetamine-Enhancer";
-  license = "mit";
-  maintainers = [ lib.maintainers.cfeeley ];
-  broken = true;
+  meta = {
+    description = "Add new abilities to the macOS keep-awake utility, Amphetamine.";
+    platforms = lib.platforms.darwin;
+    homepage = "https://github.com/x74353/Amphetamine-Enhancer";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.cfeeley ];
+  };
 }
