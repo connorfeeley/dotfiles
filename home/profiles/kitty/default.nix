bd797cc19a6fb28bc62bc069cbe8c809427b8714
Connor Feeley <cfeeley@cfeeley.org>
2022-12-15 00:00:00 -0500

    fix(nixos:hm:kitty): increase font size

--- home/profiles/kitty/default.nix
+++ home/profiles/kitty/default.nix
@@ -82,7 +82,7 @@ lib.mkMerge [
         font_size =
           if (isDarwin && isAarch64)
           then "12"
-          else "18";
+          else "24";
         confirm_os_window_close = "0";
         # if (isDarwin)
         # then "1"
