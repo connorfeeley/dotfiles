732da3cfd59de37594a450dcc6186d5167481728
Connor Feeley <connor@cfeeley.org>
2022-12-20 13:59:38 -0500

    fix(darwin): conflicting builder definition

--- darwin/profiles/virtualization/nixos-vm-host.nix
+++ darwin/profiles/virtualization/nixos-vm-host.nix
@@ -8,7 +8,7 @@ in
   nix.settings = {
     inherit substituters trusted-substituters;
     trusted-public-keys = [ "macos-builder.cachix.org-1:HPWcq59/iyqQz6HEtlO/kjD/a7ril0+/XJc+SZ2LgpI=" ];
-    builders = "ssh://builder@localhost aarch64-linux /etc/nix/builder_ed25519 4 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo='";
+    builders = [ "ssh://builder@localhost aarch64-linux /etc/nix/builder_ed25519 4 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo='" ];
     builders-use-substitutes = true;
   };
 }
