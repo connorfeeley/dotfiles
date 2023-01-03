17b72a8c8e6da7dcdf5f7d235346c39213cef123
Connor Feeley <git@cfeeley.org>
2023-01-03 11:13:22 -0500

    feat(nixos:gnome): enable all gnome services

--- nixos/profiles/gnome-desktop.nix
+++ nixos/profiles/gnome-desktop.nix
@@ -2,10 +2,29 @@
 , pkgs
 , ...
 }: {
-  # Required for Firefox integration in home-manager
-  services.gnome.gnome-browser-connector.enable = true;
-
-  services.gnome.sushi.enable = true;
+  services.gnome = {
+    # Required for Firefox integration in home-manager
+    gnome-browser-connector.enable = true;
+    sushi.enable = true; # Quick previewer for Nautilus
+    at-spi2-core.enable = true; # Service for the Assistive Technologies available on the GNOME platform
+    core-developer-tools.enable = true;
+    core-os-services.enable = true;
+    core-shell.enable = true;
+    core-utilities.enable = true;
+    evolution-data-server.enable = true;
+    games.enable = true;
+    glib-networking.enable = true;
+    gnome-initial-setup.enable = false;
+    gnome-keyring.enable = true;
+    gnome-online-accounts.enable = true;
+    gnome-online-miners.enable = true;
+    gnome-remote-desktop.enable = true;
+    gnome-settings-daemon.enable = true;
+    gnome-user-share.enable = true;
+    rygel.enable = true;
+    tracker.enable = true; # Search engine, search tool and metadata storage system.
+    tracker-miners.enable = true; # Tracker miners, indexing services for Tracker search engine and metadata storage system.
+  };
   # programs.gnupg.agent.pinentryFlavor = "gnome3";
 
   services.xserver = {
