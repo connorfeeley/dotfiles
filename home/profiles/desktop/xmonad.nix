99d3a5dfe46b3cfba2684b4f7cdc3a7c7a91f1c8
Connor Feeley <git@cfeeley.org>
2023-01-04 14:03:21 -0500

    fix?(nixos:hm): disable xmonad session

--- home/profiles/desktop/xmonad.nix
+++ home/profiles/desktop/xmonad.nix
@@ -2,18 +2,18 @@
 
 with lib;
 {
-  xsession = {
-    enable = true;
+  # xsession = {
+  #   enable = true;
 
-    windowManager.command = "${pkgs.xmonad-config}/bin/xmonad";
+  #   windowManager.command = "${pkgs.xmonad-config}/bin/xmonad";
 
-    # HM module gets in the way; Launched with windowManager.command instead.
-    windowManager.xmonad.enable = false;
+  #   # HM module gets in the way; Launched with windowManager.command instead.
+  #   windowManager.xmonad.enable = false;
 
-    initExtra = ''
-      # x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -safer -display :1
-    '';
-  };
+  #   initExtra = ''
+  #     # x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -safer -display :1
+  #   '';
+  # };
 
   home.packages = with pkgs; [
     xmonad-config
@@ -35,12 +35,13 @@ with lib;
     x11vnc
   ];
 
-  xdg.configFile = {
-    "plasma-workspace/env/set_window_manager.sh" = {
-      executable = true;
-      text = ''
-        export KDEWM="${pkgs.xmonad-config}/bin/xmonad"
-      '';
-    };
-  };
+  # For non-systemd plasma5 init:
+  # xdg.configFile = {
+  #   "plasma-workspace/env/set_window_manager.sh" = {
+  #     executable = true;
+  #     text = ''
+  #       export KDEWM="${pkgs.xmonad-config}/bin/xmonad"
+  #     '';
+  #   };
+  # };
 }
