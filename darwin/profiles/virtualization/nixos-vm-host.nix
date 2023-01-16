bcc824bfcd45e3a7aa7a3371021e13d9f769a7c5
Connor Feeley <git@cfeeley.org>
2023-01-16 12:22:10 -0500

    feat(darwin): add darwin.builder to system packages

--- darwin/profiles/virtualization/nixos-vm-host.nix
+++ darwin/profiles/virtualization/nixos-vm-host.nix
@@ -1,6 +1,8 @@
-{ ...
+{ pkgs
+, ...
 }:
 {
+  environment.systemPackages = [ pkgs.darwin.builder ];
   nix.settings = {
     builders = [ "ssh://builder@localhost aarch64-linux /etc/nix/builder_ed25519 4 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo='" ];
     builders-use-substitutes = true;
