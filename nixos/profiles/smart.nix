299a897d08030d09eeb5b0342e8bc1b6913cc542
Connor Feeley <cfeeley@cfeeley.org>
2023-01-18 00:00:00 -0500

    fix(nixos:smart): allow 'disk' group access to NVME char devices

--- nixos/profiles/smart.nix
+++ nixos/profiles/smart.nix
@@ -6,4 +6,10 @@
     enable = true;
     autodetect = true;
   };
+
+  # Allow users in the 'disk' group to access NVME character devices (char device: more raw/low-level; block device: storage).
+  # Required for smartctl prometheus exporter.
+  services.udev.extraRules = ''
+    SUBSYSTEM=="nvme", KERNEL=="nvme[0-9]*", GROUP="disk"
+  '';
 }
