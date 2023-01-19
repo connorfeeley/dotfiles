c7d979c5b927b1abbe5330fc0c53116e9e3a88b8
Connor Feeley <git@cfeeley.org>
2023-01-19 09:00:00 -0500

    fix(darwin:hmrspoon): stop stealing 'e' key

--- home/users/cfeeley/config/hammerspoon/init.lua
+++ home/users/cfeeley/config/hammerspoon/init.lua
@@ -4,8 +4,12 @@ if not ipc.cliStatus("/usr/local") then
   ipc.cliInstall()
 end
 
-hs.hotkey.bindSpec({command, "e"},
-  function ()
-    hs.task.new("zsh", nil, { "-l", "-c", "emacsclient --eval '(emacs-everywhere)'" }):start()
-  end
-)
+-- hs.hotkey.bindSpec({command, "e"},
+--   function ()
+--     hs.task.new("zsh", nil, { "-l", "-c", "emacsclient --eval '(emacs-everywhere)'" }):start()
+--   end
+-- )
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("Hello World!")
+end)
