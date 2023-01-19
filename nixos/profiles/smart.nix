0074614f1a530e2bfa6a3cab79d7467bf00a312f
Connor Feeley <cfeeley@cfeeley.org>
2023-01-19 00:00:00 -0500

    feat(nixos:smart): set extra options

--- nixos/profiles/smart.nix
+++ nixos/profiles/smart.nix
@@ -5,6 +5,11 @@
   services.smartd = {
     enable = true;
     autodetect = true;
+    extraOptions = [
+      "-A /var/log/smartd/"
+      # "--interval=3600"
+      "--interval=60"
+    ];
   };
 
   # Allow users in the 'disk' group to access NVME character devices (char device: more raw/low-level; block device: storage).
