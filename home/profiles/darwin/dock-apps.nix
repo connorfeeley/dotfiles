cea8ee41d293b746131a8c85d3bf19fa2cdd8a99
Connor Feeley <git@cfeeley.org>
2023-01-20 09:00:00 -0500

    fix(darwin:dock): add items using absolute path

--- home/profiles/darwin/dock-apps.nix
+++ home/profiles/darwin/dock-apps.nix
@@ -12,7 +12,7 @@ in
   home.activation.updateDock =
     let
       homeDir = "/Users/${guardian.username}/Library/Preferences/com.apple.dock.plist";
-      replaceAppEntry = app: ''$DRY_RUN_CMD ${pkgs.dockutil}/bin/dockutil --add "file:///etc/profiles/per-user/cfeeley/Applications/${app}.app" --replacing ${app} ${homeDir}'';
+      replaceAppEntry = app: ''$DRY_RUN_CMD ${pkgs.dockutil}/bin/dockutil --add "file://$(readlink -f "/etc/profiles/per-user/cfeeley/Applications/${app}.app")" --replacing ${app} ${homeDir}'';
       maybeEmacsCommand = lib.optionalString config.programs.emacs.enable (replaceAppEntry "Emacs");
       maybeItermCommand = lib.optionalString config.programs.iterm2.enable (replaceAppEntry "iTerm2");
     in
