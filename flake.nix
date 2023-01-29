485f695a186e0f88ccddc6a8c2d85386adcac7bf
Connor Feeley <git@cfeeley.org>
2023-01-29 09:00:00 -0500

    feat: update nixpkgs-darwin input

--- flake.nix
+++ flake.nix
@@ -158,6 +158,7 @@
       ];
 
       collective = {
+        containers = importExportableModules ./containers;
         modules = importExportableModules ./modules;
         peers = import ./ops/metadata/peers.nix;
         profiles = rakeLeaves ./profiles;
