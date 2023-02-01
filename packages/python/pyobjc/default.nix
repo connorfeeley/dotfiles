0d423ef49f952a13d0f99e748160ad5676259889
Connor Feeley <git@cfeeley.org>
2023-02-01 09:00:00 -0500

    refact(pyobjc): generate disabled tests

--- packages/python/pyobjc/default.nix
+++ packages/python/pyobjc/default.nix
@@ -20,13 +20,17 @@ let
     , extraBuildInputs ? [ ]
     , frameworkInputs ? [ ]
     , checkInputs ? [ ]
-    , preConfigure ? ""
-    , preCheck ? ""
+    , preConfigure ? preConfigureDefault
     }:
     callPackage ./generic.nix {
-      inherit pname pythonImportsCheck pytestFlagsArray disabledTestPaths disabledTests doCheck extraBuildInputs frameworkInputs preConfigure preCheck checkInputs;
+      inherit pname pythonImportsCheck pytestFlagsArray disabledTestPaths disabledTests doCheck extraBuildInputs frameworkInputs checkInputs;
 
       inherit lib stdenv python3Packages fetchFromGitHub libffi darwin frameworks xcbuild xcbuildHook;
+
+      preCheck = generateSubstitutions {
+        substitutionFiles = [ ''$(find PyObjCTest -type f -name "*.py")'' ];
+        substitutions = disableTests disabledTests;
+      };
     };
 
   substitute = old: new: "--replace '${old}' '${new}'";
@@ -41,8 +45,32 @@ let
 
     IFS="$OIFS"
   '';
-
   disableTests = builtins.map (test: substitute "def ${test}(self)" "def _${test}(self)");
+
+  # Base preConfigure: applies to all packages
+  preConfigureCore = generateSubstitutions {
+    substitutionFiles = [ ''$(find . -type f -name "*.py")'' ];
+    substitutions = [
+      (substitute ''["/usr/bin/xcrun", "-sdk", "macosx", "--show-sdk-path"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK}"]'')
+      (substitute ''["/usr/bin/sw_vers", "-productVersion"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK.passthru.version}"]'')
+      (substitute ''assert sdkname.startswith("MacOSX")'' "")
+      (substitute ''assert sdkname.endswith(".sdk")'' "")
+      (substitute ''get_sdk_level(self.sdk_root)'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}"'')
+    ];
+  };
+  # Default preConfigure: applies to all packages except pyobjc-core
+  preConfigureDefault = generateSubstitutions {
+    inherit (preConfigureCore) substitutionFiles;
+    substitutions = preConfigureCore.substitutions ++ [
+        (substitute ''version.split(".")'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}".split(".")'')
+    ];
+  };
+
+  # Remaining framework wrappers - can be installed in any order (after pyobjc-core, pyobjc-framework-Cocoa, and pyobjc-framework-Quartz)
+  frameworkWrappers =
+    let all = (import ./framework-wrappers.nix).FRAMEWORK_WRAPPERS;
+    in
+    lib.filter (e: e.name != "Cocoa" && e.name != "Quartz") all;
 in
 rec {
   # Install order ():
@@ -50,91 +78,53 @@ rec {
   # - pyobjc-framework-Cocoa
   # - pyobjc-framework-Quartz
 
-  # nix-build --pure ~dots -A packages.aarch64-darwin.pyobjc.pyobjc-core --show-trace
+  ###
+  ### Core package
+  ###   nix-build --pure ~dots -A packages.aarch64-darwin.pyobjc.pyobjc-core
   pyobjc-core = (mkPackage {
     pname = "pyobjc-core";
     pythonImportsCheck = [ "objc._objc" ];
 
+    preConfigure = preConfigureCore;
     extraBuildInputs = [ darwin.DarwinTools ];
     frameworkInputs = [ darwin.apple_sdk.objc4 ];
+  }).overrideAttrs (old: {
+    setupPyBuildFlags = old.setupPyBuildFlags ++ [ "--no-lto" ];
+  });
 
-    preConfigure = generateSubstitutions {
-      substitutionFiles = [ ''$(find . -type f -name "*.py")'' ];
-      substitutions = [
-        (substitute ''["/usr/bin/xcrun", "-sdk", "macosx", "--show-sdk-path"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK}"]'')
-        (substitute ''["/usr/bin/sw_vers", "-productVersion"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK.passthru.version}"]'')
-        (substitute ''assert sdkname.startswith("MacOSX")'' "")
-        (substitute ''assert sdkname.endswith(".sdk")'' "")
-        (substitute ''get_sdk_level(self.sdk_root)'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}"'')
-      ];
-    };
-
-    preCheck = generateSubstitutions {
-      substitutionFiles = [ "PyObjCTest/test_bundleFunctions.py" ];
-      substitutions = [
-        (substitute ''self.assertEqual(value, os.path.expanduser("~"))'' ''self.assertEqual(value, value)'')
-      ];
-    };
-  }).overrideAttrs (old: { setupPyBuildFlags = old.setupPyBuildFlags ++ [ "--no-lto" ]; });
-
+  ###
+  ### Cocoa framework
+  ###   nix-build --pure ~dots -A packages.aarch64-darwin.pyobjc.pyobjc-framework-Cocoa
   pyobjc-framework-Cocoa = mkPackage {
     pname = "pyobjc-framework-Cocoa";
     pythonImportsCheck = [ "Cocoa" ];
 
-    preConfigure = generateSubstitutions {
-      substitutionFiles = [ ''$(find . -type f -name "*.py")'' ];
-      substitutions = [
-        (substitute ''["/usr/bin/xcrun", "-sdk", "macosx", "--show-sdk-path"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK}"]'')
-        (substitute ''["/usr/bin/sw_vers", "-productVersion"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK.passthru.version}"]'')
-        (substitute ''assert sdkname.startswith("MacOSX")'' "")
-        (substitute ''assert sdkname.endswith(".sdk")'' "")
-        (substitute ''get_sdk_level(self.sdk_root)'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}"'')
-        (substitute ''version.split(".")'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}".split(".")'')
-      ];
-    };
-
-    preCheck = generateSubstitutions {
-      substitutionFiles = [ ''$(find PyObjCTest -type f -name "*.py")'' ];
-      substitutions = disableTests [
-        "test_subclassing"
-        "test_issue_272"
-        "testGetting"
-        "testSetting"
-        "test_issue282"
-        "testFunctions"
-      ];
-    };
+    disabledTests = [
+      "test_subclassing"
+      "test_issue_272"
+      "testGetting"
+      "testSetting"
+      "test_issue282"
+      "testFunctions"
+    ];
 
     extraBuildInputs = [ pyobjc-core ];
     frameworkInputs = [ frameworks.Cocoa ];
   };
 
-  # nix-build --pure $DOTFIELD_DIR -A packages.aarch64-darwin.pyobjc.pyobjc-framework-Quartz --show-trace --verbose --keep-failed
+  ###
+  ### Quartz framework
+  ###   nix-build --pure ~dots -A packages.aarch64-darwin.pyobjc.pyobjc-framework-Quartz
   pyobjc-framework-Quartz = mkPackage {
     pname = "pyobjc-framework-Quartz";
     pythonImportsCheck = [ "Quartz" ];
 
-    preConfigure = generateSubstitutions {
-      substitutionFiles = [ ''$(find . -type f -name "*.py")'' ];
-      substitutions = [
-        (substitute ''["/usr/bin/xcrun", "-sdk", "macosx", "--show-sdk-path"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK}"]'')
-        (substitute ''["/usr/bin/sw_vers", "-productVersion"]'' ''["echo", "${darwin.apple_sdk.MacOSX-SDK.passthru.version}"]'')
-        (substitute ''assert sdkname.startswith("MacOSX")'' "")
-        (substitute ''assert sdkname.endswith(".sdk")'' "")
-        (substitute ''get_sdk_level(self.sdk_root)'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}"'')
-        (substitute ''version.split(".")'' ''"${darwin.apple_sdk.MacOSX-SDK.passthru.version}".split(".")'')
-      ];
-    };
-
-    preCheck = generateSubstitutions {
-      substitutionFiles = [ ''$(find PyObjCTest -type f -name "*.py")'' ];
-      substitutions = disableTests [
-        "test_callable_metadata_is_sane"
-        "test_protocols"
-        "testFunctions"
-        "testConstants10_6"
-      ];
-    };
+    disabledTests = [
+      "test_callable_metadata_is_sane"
+      "test_protocols"
+      "testFunctions"
+      "testConstants10_6"
+    ];
 
     extraBuildInputs = [ pyobjc-core pyobjc-framework-Cocoa ];
     frameworkInputs = [
