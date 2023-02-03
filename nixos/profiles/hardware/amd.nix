523870c1557e5be93dd24dc03b814fbd1f5941d3
Connor Feeley <git@cfeeley.org>
2023-02-03 09:00:00 -0500

    amd: enable ryzen SMU (system monitor unit) driver

--- nixos/profiles/hardware/amd.nix
+++ nixos/profiles/hardware/amd.nix
@@ -1,8 +1,16 @@
 { config
 , lib
+, pkgs
 , ...
 }: lib.mkIf (!config.nixos-vm.enable) {
   boot.kernelModules = [ "kvm-amd" ];
   boot.initrd.kernelModules = [ "kvm-amd" ];
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Driver that exposes access to the SMU (System Management Unit) for certain AMD Ryzen Processors
+  boot.extraModulePackages = [
+    (pkgs.nur.repos.arc.packages.ryzen-smu.override {
+      linux = config.boot.kernelPackages.kernel;
+    })
+  ];
 }
