6f1e05a40243bf8f32bd8e495b35ad3e6c354d07
Connor Feeley <git@cfeeley.org>
2023-02-09 02:48:56 -0500

    flake: convert to flake-parts multi-module example

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     ##: --- system -------------------------------------------------------------
     home-manager = { url = "github:nix-community/home-manager/release-22.11"; inputs.nixpkgs.follows = "nixpkgs"; };
     darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-parts = { url = "github:hercules-ci/flake-parts"; };
     digga = {
       url = "github:divnix/digga";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -31,8 +32,8 @@
 
     ##: --- utilities ----------------------------------------------------------
     flake-utils.url = "github:numtide/flake-utils";
-    flake-parts = { url = "github:hercules-ci/flake-parts"; };
 
+    ##: --- other packages -----------------------------------------------------
     nur.url = "github:nix-community/NUR";
     nixos-generators = { url = "github:nix-community/nixos-generators"; inputs.nixpkgs.follows = "nixpkgs"; };
     nvfetcher.url = "github:berberman/nvfetcher";
@@ -139,11 +140,6 @@
     , ...
     } @ inputs:
     let
-      inherit (digga.lib)
-        flattenTree
-        importExportableModules
-        rakeLeaves
-        ;
       inherit (flake-utils.lib)
         eachSystem
         ;
@@ -162,282 +158,25 @@
         x86_64-darwin
         aarch64-darwin
       ];
-
-      collective = {
-        modules = importExportableModules ./modules;
-        peers = import ./ops/metadata/peers.nix;
-        profiles = rakeLeaves ./profiles;
-      };
-
-      # FIXME: split this to shared/nixos/darwin-specific
-      overlays = [
-        agenix.overlay
-        emacs-overlay.overlay
-        gitignore.overlay
-        nur.overlay
-        nvfetcher.overlays.default
-
-        nix-xilinx.overlay
-
-        (final: _prev:
-          let
-            packagesFrom = inputAttr: inputAttr.packages.${final.system};
-          in
-          {
-            inherit (packagesFrom self.packages) emacs-plus;
-            inherit (packagesFrom inputs.devenv) devenv;
-            inherit (packagesFrom inputs.deploy) deploy-rs;
-            inherit (packagesFrom inputs.deploy-flake) deploy-flake;
-            inherit (packagesFrom inputs.prefmanager) prefmanager;
-            inherit (packagesFrom inputs.nix-nil) nil;
-            inherit (packagesFrom inputs.nix-alien) nix-alien;
-            inherit (packagesFrom inputs.nix-alien) nix-index-update;
-            inherit (packagesFrom inputs.nix-autobahn) nix-autobahn;
-
-            # Personal packages
-            inherit (packagesFrom inputs.nurpkgs) apple_complete maclaunch;
-            inherit (packagesFrom inputs.xmonad-config) xmonad-config;
-            inherit (packagesFrom inputs.ttc-subway-font) ttc-subway bloor-yonge-font;
-
-            inherit (inputs.nixpkgs-input-leap.legacyPackages.${final.system}) input-leap;
-
-            nix-init = inputs.nix-init.packages.${final.system}.default;
-            emacsGitDarwin = inputs.darwin-emacs.packages.${final.system}.default;
-            neovim-plusultra = inputs.neovim-plusultra.packages.${final.system}.neovim;
-          }
-        )
-        (import ./overlays/tum-dse-config { inherit inputs; })
-        (import ./overlays/python { inherit inputs; })
-      ];
-
-      commonImports = [
-        (digga.lib.importOverlays ./overlays/common)
-        (digga.lib.importOverlays ./packages)
-      ];
     in
-    (digga.lib.mkFlake {
-      inherit
-        self
-        inputs
-        supportedSystems
-        ;
-
-      channelsConfig = {
-        allowUnfree = true;
-        allowUnsupportedSystem = false;
-
-        allowBroken = false;
-
-        permittedInsecurePackages = [
-          # nixpkgs: pkgs/development/tools/poetry2nix/poetry2nix/mk-poetry-dep.nix
-          "python2.7-pyjwt-1.7.1"
-          "python3.10-poetry-1.2.2"
-          "python3.10-certifi-2022.9.24"
-        ];
-      };
-
-      ###
-      ### Overlay & Channel Configuration
-      ###
-      channels = {
-        nixos-stable = {
-          inherit overlays;
-          imports = commonImports ++
-          [
-            (digga.lib.importOverlays ./overlays/stable)
-          ];
-        };
-        nixpkgs-darwin = {
-          imports = commonImports ++
-          [
-            (digga.lib.importOverlays ./overlays/nixpkgs-darwin)
-            (digga.lib.importOverlays ./overlays/stable)
-          ];
-          overlays =
-            overlays ++ [
-              (final: _prev: {
-                amphetamine-enhancer = self.packages.${final.system}.amphetamine-enhancer;
-                mints = self.packages.${final.system}.mints;
-                hammerspoon = self.packages.${final.system}.hammerspoon;
-                native-youtube = self.packages.${final.system}.native-youtube;
-              })
-            ];
-        };
-        nixos-unstable = {
-          inherit overlays;
-
-          imports = commonImports ++
-          [
-            (digga.lib.importOverlays ./overlays/nixos-unstable)
-          ];
-        };
-      };
-
-      sharedOverlays = [
-        (_final: prev: {
-          __dontExport = true;
-          inherit inputs;
-          lib = prev.lib.extend (_lfinal: _lprev: {
-            our = self.lib;
-          });
-        })
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./nixos/flake-module.nix
       ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = { config, self', inputs', ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
 
-      ###
-      ### Other attributes
-      ###
-      lib = import ./lib {
-        inherit collective;
-        lib = digga.lib // nixos-unstable.lib;
+        packages.figlet = inputs'.nixpkgs.legacyPackages.figlet;
       };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
 
-      nixos = import ./nixos collective;
-      darwin = import ./darwin collective;
-      home = import ./home collective;
-
-      devshell = ./shell;
-
-      homeConfigurations = digga.lib.mergeAny
-        (digga.lib.mkHomeConfigurations self.darwinConfigurations)
-        (digga.lib.mkHomeConfigurations self.nixosConfigurations)
-      ;
-
-      colmena = import ./colmena.nix { inherit nixpkgs inputs self; };
-
-      deploy.nodes = digga.lib.mkDeployNodes { } {
-        workstation = {
-          hostname = "workstation";
-          sshUser = "cfeeley";
-          remoteBuild = true;
-          fastConnection = true;
-          autoRollback = true;
-          magicRollback = true;
-          profiles.cfeeley = {
-            user = "cfeeley";
-            path = deploy.lib.x86_64-linux.activate.nixos
-              self.nixosConfigurations.workstation;
-          };
-        };
-        rosy = {
-          hostname = "rosy";
-          sshUser = "cfeeley";
-          remoteBuild = true;
-          fastConnection = true;
-          autoRollback = true;
-          magicRollback = true;
-          profiles.system = {
-            user = "root";
-            path = deploy.lib.aarch64-linux.activate.nixos
-              self.nixosConfigurations.rosy;
-          };
-          profiles.cfeeley = {
-            user = "cfeeley";
-            path = deploy.lib.aarch64-linux.activate.home-manager
-              self.homeConfigurations."cfeeley@rosy";
-          };
-        };
-        h8tsner = {
-          hostname = "h8tsner";
-          sshUser = "root";
-          sshOpts = [ "-p" "26473" ];
-          fastConnection = false;
-          autoRollback = true;
-          magicRollback = true;
-          profiles.system = {
-            user = "root";
-            path = deploy.lib.x86_64-linux.activate.nixos
-              self.nixosConfigurations.h8tsner;
-          };
-          profiles.cfeeley = {
-            user = "cfeeley";
-            path = deploy.lib.x86_64-linux.activate.home-manager
-              self.homeConfigurations."cfeeley@h8tsner";
-          };
-        };
-        # Deploy to 'cfeeley-laptop':
-        # - Recommended: deploy .#cfeeley-laptop -- --print-build-logs
-        # If there are nix eval errors, then we can tell 'deploy' to skip the flake checks:
-        # - Not recommended: deploy --skip-checks .#cfeeley-laptop -- --print-build-logs
-        cfeeley-laptop = with (collective.peers.hosts.cfeeley-laptop); {
-          hostname = ipv4.address;
-          sshUser = "cfeeley";
-          fastConnection = true;
-          autoRollback = true;
-          magicRollback = true;
-          profilesOrder = [ "cfeeley" ];
-          profiles.cfeeley = {
-            user = "cfeeley";
-            path = deploy.lib.x86_64-linux.activate.home-manager
-              self.homeConfigurationsPortable.x86_64-linux."cfeeley@cfeeley-laptop";
-          };
-        };
       };
-
-      overlays = rec {
-        # Helper function to install DMGs
-        installApplication = self.overlays."nixpkgs-darwin/installApplication";
-        darwin-packages = nixpkgs.lib.composeManyExtensions [
-          installApplication
-
-          self.overlays."nixpkgs-darwin/emacs28Macport"
-          self.overlays."nixpkgs-darwin/emacs-plus"
-          self.overlays."nixpkgs-darwin/macports"
-        ];
-        linux-packages = nixpkgs.lib.composeManyExtensions [
-          # FIXME(darwin): causes 'nix flake show' to error
-          # self.overlays."nixos-stable/xmonad-config"
-
-          self.overlays."nixos-stable/xsct"
-          self.overlays."nixos-stable/mdio-tools"
-          self.overlays."nixos-stable/aranet4"
-          # self.overlays."nixos-stable/fildem-global-menu"
-        ];
-      };
-    })
-    //
-    # Generate attrs for each system: (formatter.<system>)
-    (eachSystem supportedSystems (system: {
-      formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-    })) //
-    # Generate attrs for each system: (formatter.<system>)
-    {
-      packages =
-        let
-          mkLinuxPackages = system:
-            let
-              pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.linux-packages ]; config.allowUnfree = true; };
-            in
-            {
-              # FIXME(darwin): causes 'nix flake show' to error
-              inherit (pkgs) xsct mdio-tools aranet4;
-            };
-
-          mkDarwinPackages = system:
-            let
-              pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.darwin-packages ]; config.allowUnfree = true; };
-            in
-            {
-              inherit (pkgs)
-                macports
-                amphetamine-enhancer
-                mints
-                hammerspoon
-                native-youtube
-                emacs28Macport
-                ;
-
-              # - Should most likely not be used as part of a system configuration (use emacs28Macport instead)
-              emacs28Macport-noNativeComp = pkgs.emacs28Macport.override { nativeComp = false; };
-            } //
-            (builtins.mapAttrs (_n: v: pkgs.callPackage v { inherit (pkgs) installApplication; })
-              (flattenTree (rakeLeaves ./darwin/packages)));
-        in
-        {
-          x86_64-linux = mkLinuxPackages "x86_64-linux";
-          aarch64-linux = mkLinuxPackages "aarch64-linux";
-          x86_64-darwin = mkDarwinPackages "x86_64-darwin";
-          aarch64-darwin = mkDarwinPackages "aarch64-darwin";
-        };
     };
 
   # Automatic nix.conf settings (accepted automatically when 'accept-flake-config = true')
---
+++ nixos/flake-module.nix
@@ -0,0 +1,17 @@
+# Definitions can be imported from a separate file like this one
+
+{ self, lib, ... }: {
+  perSystem = { config, self', inputs', pkgs, ... }: {
+    # Definitions like this are entirely equivalent to the ones
+    # you may have directly in flake.nix.
+    packages.hello = pkgs.hello;
+  };
+  flake = {
+    nixosModules.hello = { pkgs, ... }: {
+      environment.systemPackages = [
+        # or self.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.hello
+        self.packages.${pkgs.stdenv.hostPlatform.system}.hello
+      ];
+    };
+  };
+}
