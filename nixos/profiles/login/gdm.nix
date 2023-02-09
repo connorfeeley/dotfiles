2385689476c32a19a6f75c579edcfdac175f5e37
Connor Feeley <git@cfeeley.org>
2023-02-09 16:10:16 -0500

    gdm: unlock gnome keyring on login

--- nixos/profiles/login/gdm.nix
+++ nixos/profiles/login/gdm.nix
@@ -1,6 +1,7 @@
-{ config, ...}: {
+{ config, ... }:
+{
+  security.pam.services.gdm.enableGnomeKeyring = true;
   services.xserver = {
-
     displayManager.gdm = {
       enable = true;
       # Don't autosuspend from GDM
