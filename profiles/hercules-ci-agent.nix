c2a5f308659e72a1a407148092d22ad106d0218c
Connor Feeley <git@cfeeley.org>
2023-02-09 09:00:00 -0500

    ci: set hercules-ci agent label/tag

--- profiles/hercules-ci-agent.nix
+++ profiles/hercules-ci-agent.nix
@@ -1,6 +1,9 @@
 { inputs, config, pkgs, lib, ... }:
 {
   services.hercules-ci-agent.enable = true;
+  services.hercules-ci-agent.settings = {
+    labels.tags = [ pkgs.stdenv.system ];
+  };
 
   environment.systemPackages = [ pkgs.hci ];
 }
