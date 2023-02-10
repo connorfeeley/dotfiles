f6b320718784fae2e88d853ecc436b03ac8f5f50
Connor Feeley <git@cfeeley.org>
2023-02-10 09:00:00 -0500

    git: actually include the git worktree script

--- home/profiles/git.nix
+++ home/profiles/git.nix
@@ -193,8 +193,45 @@ in
       ds = "diff --staged";
       amend = "commit --amend";
       clone-for-worktrees =
-        let git-clone-bare-for-worktrees = pkgs.writeShellScript "git-clone-bare-for-worktrees" '''';
-        in "!sh ${git-clone-bare-for-worktrees}";
+        let
+          # https://morgan.cugerone.com/blog/workarounds-to-git-worktree-using-bare-repository-and-cannot-fetch-remote-branches/
+          git-clone-bare-for-worktrees = pkgs.writeShellScript "git-clone-bare-for-worktrees" ''
+            set -e
+
+            # Examples of call:
+            # git-clone-bare-for-worktrees git@github.com:name/repo.git
+            # => Clones to a /repo directory
+            #
+            # git-clone-bare-for-worktrees git@github.com:name/repo.git my-repo
+            # => Clones to a /my-repo directory
+
+            url=$1
+            basename=''${url##*/}
+            name=''${2:-''${basename%.*}}
+
+            mkdir $name
+            cd "$name"
+
+            # Moves all the administrative git files (a.k.a $GIT_DIR) under .bare directory.
+            #
+            # Plan is to create worktrees as siblings of this directory.
+            # Example targeted structure:
+            # .bare
+            # main
+            # new-awesome-feature
+            # hotfix-bug-12
+            # ...
+            git clone --bare "$url" .bare
+            echo "gitdir: ./.bare" > .git
+
+            # Explicitly sets the remote origin fetch so we can fetch remote branches
+            git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+            # Gets all branches from origin
+            git fetch origin
+          '';
+        in
+        "!sh ${git-clone-bare-for-worktrees}";
     };
 
     extraConfig = lib.mkMerge [
