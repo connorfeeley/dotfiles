30a04ae6009a648ca3e68bf631a56d13df6a8b36
Connor Feeley <git@cfeeley.org>
2023-02-12 19:03:44 -0500

    home: enable htop

--- home/profiles/core/default.nix
+++ home/profiles/core/default.nix
@@ -129,6 +129,41 @@ in
   # across various tools. See the home-manager manual for more info.
   programs.man.generateCaches = lib.mkDefault true;
   programs.nix-index.enable = true;
+  programs.htop = {
+    enable = true;
+    settings = {
+      color_scheme = 6;
+      cpu_count_from_one = 0;
+      delay = 15;
+      fields = with config.lib.htop.fields; [
+        PID
+        USER
+        PRIORITY
+        NICE
+        M_SIZE
+        M_RESIDENT
+        M_SHARE
+        STATE
+        PERCENT_CPU
+        PERCENT_MEM
+        TIME
+        COMM
+      ];
+      highlight_base_name = 1;
+      highlight_megabytes = 1;
+      highlight_threads = 1;
+    } // (with config.lib.htop; leftMeters [
+      (bar "AllCPUs2")
+      (bar "Memory")
+      (bar "Swap")
+      (text "Zram")
+    ]) // (with config.lib.htop; rightMeters [
+      (text "Tasks")
+      (text "LoadAverage")
+      (text "Uptime")
+      (text "Systemd")
+    ]);
+  };
 
   home.enableDebugInfo = true;
   home.enableNixpkgsReleaseCheck = true;
