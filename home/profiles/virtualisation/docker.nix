ead82d2a58e709cbe3432f68b25d4a377e76626e
Connor Feeley <git@cfeeley.org>
2023-02-28 10:59:26 -0500

    home(docker): forcibly create symlinks

--- home/profiles/virtualisation/docker.nix
+++ home/profiles/virtualisation/docker.nix
@@ -4,8 +4,8 @@ let inherit (config.lib.dag) entryAfter; in {
   home.activation.setupDockerCLI = entryAfter [ "writeBoundary" ] ''
     DOCKER_CONFIG=''${DOCKER_CONFIG:-$HOME/.docker}
     mkdir -p $DOCKER_CONFIG/cli-plugins
-    ln -s $(which docker-compose) $DOCKER_CONFIG/cli-plugins/docker-compose
-    ln -s $(which docker-buildx) $DOCKER_CONFIG/cli-plugins/docker-buildx
+    ln -sf $(which docker-compose) $DOCKER_CONFIG/cli-plugins/docker-compose
+    ln -sf $(which docker-buildx) $DOCKER_CONFIG/cli-plugins/docker-buildx
   '';
   home.packages = with pkgs;
     [ docker-buildx docker-credential-helpers buildkit ] ++ lib.optionals
