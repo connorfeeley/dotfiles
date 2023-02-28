3a1332125ef31ae47bf58da0710fc749bb5226ec
Connor Feeley <git@cfeeley.org>
2023-02-28 09:56:35 -0500

    home(docker): set up docker CLI plugins automatically

--- home/profiles/virtualisation/docker.nix
+++ home/profiles/virtualisation/docker.nix
@@ -1,10 +1,12 @@
-moduleArgs@{ config, lib, pkgs, ... }: {
-  /* Setup docker CLI plugins:
-     DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-     mkdir -p $DOCKER_CONFIG/cli-plugins
-     ln -s $(which docker-compose) $DOCKER_CONFIG/cli-plugins/docker-compose
-     ln -s $(which docker-buildx) $DOCKER_CONFIG/cli-plugins/docker-buildx
-  */
+moduleArgs@{ config, lib, pkgs, ... }:
+let inherit (config.lib.dag) entryAfter; in {
+  # Setup docker CLI plugins:
+  home.activation.setupDockerCLI = entryAfter [ "writeBoundary" ] ''
+    DOCKER_CONFIG=''${DOCKER_CONFIG:-$HOME/.docker}
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    ln -s $(which docker-compose) $DOCKER_CONFIG/cli-plugins/docker-compose
+    ln -s $(which docker-buildx) $DOCKER_CONFIG/cli-plugins/docker-buildx
+  '';
   home.packages = with pkgs;
     [ docker-buildx docker-credential-helpers buildkit ] ++ lib.optionals
       (moduleArgs.osConfig.virtualisation.docker.enable or false) [
