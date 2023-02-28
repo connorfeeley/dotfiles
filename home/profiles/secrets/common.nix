5a143129f219324b0c3b32f27717e8fa583c2726
Connor Feeley <git@cfeeley.org>
2023-02-28 13:05:39 -0500

    home(secrets): add rbw bitwarden client

--- home/profiles/secrets/common.nix
+++ home/profiles/secrets/common.nix
@@ -1,3 +1,9 @@
 { config, lib, ... }:
 let inherit (config.lib) dotfield;
-in { home.sessionVariables.AGENIX_ROOT = "${dotfield.fsPath}/secrets/age"; }
+in {
+  home.sessionVariables.AGENIX_ROOT = "${dotfield.fsPath}/secrets/age";
+
+  # Bitwarden CLI
+  programs.rbw.enable = true;
+  programs.rbw.settings.email = "${lib.strings.toLower config.lib.dotfield.whoami.firstName}@${config.lib.dotfield.whoami.domain}";
+}
