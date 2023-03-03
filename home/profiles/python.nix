6f86947a5d9171b69a421b326528f075a1831ea4
Connor Feeley <git@cfeeley.org>
2023-03-03 06:34:40 -0500

    python: (disabled) openai-whisper with CUDA

--- home/profiles/python.nix
+++ home/profiles/python.nix
@@ -7,5 +7,6 @@
       # lsp server
       pyright
       openai
+      # (openai-whisper.override ({ torch = pkgs.python3.pkgs.torchWithCuda; }))
     ] ++ (with python3Packages; [ ipython black ]);
 }
