c643e1f5c18b7e6e3910906b508363986d06d9f5
Connor Feeley <git@cfeeley.org>
2023-03-11 03:04:50 -0500

    home(shells): customize nushell

--- home/profiles/shells/nushell/default.nix
+++ home/profiles/shells/nushell/default.nix
@@ -1,11 +1,29 @@
-{ ... }: {
+{ pkgs, ... }: {
+  home.packages = [
+    pkgs.jc # convert output of popular CLI tools to JSON
+  ];
   programs.nushell = {
     enable = true;
     configFile.text = ''
-      let $config = {
-        filesize_metric: false
+      let-env config = {
+        rm_always_trash: true
+        completion_algorithm: fuzzy
+        show_banner: true
+
+        filesize_metric: true
         table_mode: rounded
         use_ls_colors: true
+
+        hooks : {
+          pre_prompt: [{
+            code: "
+              let direnv = (direnv export json | from json)
+              let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
+              $direnv | load-env
+            "
+          }]
+        }
+        alias cat = bat
       }
     '';
   };
