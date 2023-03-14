ca12341563b51350407b40171d61bc7352fe9191
Connor Feeley <git@cfeeley.org>
2023-03-14 18:57:34 -0400

    mail: fixed msmtp configuration

--- nixos/profiles/mail.nix
+++ nixos/profiles/mail.nix
@@ -2,19 +2,36 @@
 let
   inherit (config.lib.dotfield.whoami) domain;
   inherit (config.lib.dotfield.secrets) mkAgeSecret;
+  inherit (config.dotfield) guardian;
 in
 {
+  #
   age.secrets = lib.mkMerge [ (mkAgeSecret "fastmail.txt") ];
   programs.msmtp = {
     enable = true;
     setSendmail = true;
-    accounts.default = {
+    accounts.fastmail = {
       auth = true;
       tls = true;
+      tls_starttls = false;
+      port = 465;
       from = "automated@cfeeley.org";
       host = "mail.messagingengine.com";
       passwordeval = "cat ${config.age.secrets."fastmail.txt".path}";
-      user = "cfeeley.org";
+      user = "connor@cfeeley.org";
     };
+    extraConfig = ''
+      account default : fastmail
+    '';
+    defaults.aliases = "/etc/aliases";
   };
+
+  environment.etc."aliases" = {
+    text = ''
+      root: automated@cfeeley.org
+    '';
+    mode = "0644";
+  };
+
+  users.users.${guardian.username}.extraGroups = [ "mail" ];
 }
