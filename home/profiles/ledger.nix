e8645d0a7320de8ad432613c97bb8d54aca94053
Connor Feeley <git@cfeeley.org>
2023-03-15 18:26:37 -0400

    home(ledger): add ledger-autosync

--- home/profiles/ledger.nix
+++ home/profiles/ledger.nix
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 { pkgs, ... }: {
+  home.packages = [ pkgs.ledger-autosync ];
   programs.ledger.enable = true;
   programs.ledger.package = pkgs.ledger;
   programs.ledger.extraConfig = ''
