2483f0baa693af98df260ce3dd400952e4d81cad
Connor Feeley <git@cfeeley.org>
2023-03-15 02:15:18 -0400

    home(linux): add strace

--- home/profiles/development/tools.nix
+++ home/profiles/development/tools.nix
@@ -69,6 +69,7 @@ in
     ] ++ (lib.optionals isLinux [
       nixpkgs-update # <- swiss-army knife for updating nix packages
       kgraphviewer # <- KDE-flavoured Graphviz viewer
+      strace # <- trace system calls and signals
     ]) ++ (lib.optionals (isLinux && !isAarch64) [
       postman # <- GNU Make with comprehensible tracing and a debugger
     ]);
