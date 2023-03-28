bf37b16dcacdfb2440c3260998d29734ddaed926
Connor Feeley <git@cfeeley.org>
2023-03-28 13:50:50 -0400

    home(desktop): fix vnc service

--- home/profiles/desktop/vnc.nix
+++ home/profiles/desktop/vnc.nix
@@ -6,19 +6,21 @@
 
 let
   inherit (pkgs.stdenv) isLinux isDarwin isAarch64;
-  inherit (config.home-manager.users.cfeeley.home) homeDirectory;
+  inherit (config.home) homeDirectory;
 in
 {
   systemd.user.services = {
     x11-server = {
       Unit = {
         Description = "x11-server";
+        After = [ "display-manager.service" "network.target" "syslog.target"];
         PartOf = [ "multi-user.target" ];
       };
       Service = {
         Type = "simple";
-        ExecStart = "${pkgs.x11vnc}/bin/x11vnc -display :0 -auth guess -forever -noxdamage -repeat -shared -rbfauth ${homeDirectory}/.vnc/passwd -rfbport 5900";
-        Restart = "always";
+        ExecStart = "${pkgs.x11vnc}/bin/x11vnc -display :0 -auth /run/user/1000/gdm/Xauthority -forever -noxdamage -repeat -shared -passwdfile ${homeDirectory}/.vnc/passwd -rfbport 5900 -nap -many";
+        ExecStop = "${pkgs.x11vnc}/bin/x11vnc -R stop";
+        Restart= "on-failure";
       };
     };
   };
