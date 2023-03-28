edb9818c9b36007e5f2fd7ac9355ce7940b9c533
Connor Feeley <git@cfeeley.org>
2023-03-27 20:23:32 -0400

    nixos(binary-cache): auto-create store keys

--- nixos/profiles/binary-cache.nix
+++ nixos/profiles/binary-cache.nix
@@ -4,4 +4,18 @@
     enable = true;
     secretKeyFile = "/etc/nix/cache-priv-key.pem";
   };
+
+  systemd.services.generate-nix-cache-key = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    path = [ pkgs.nix ];
+    script = ''
+      [[ -f /etc/nix/cache-priv-key.pem ]] && exit
+      nix-store --generate-binary-cache-key ${config.networking.hostName}-1 /etc/nix/cache-priv-key.pem /etc/nix/cache-pub-key.pem
+    '';
+
+  };
+  nix.extraOptions = ''
+    secret-key-files = /etc/nix/cache-priv-key.pem
+  '';
 }
