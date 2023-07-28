2689abf5ccbe7cc1a3acf87400ec4abb07c896c7
Connor Feeley <git@cfeeley.org>
2023-07-28 16:46:51 -0400

    darwin|home: use awscli2
    (cherry picked from commit 2d887549070844735c65ab2e50e5ac86275e33fd)

--- home/profiles/aws.nix
+++ home/profiles/aws.nix
@@ -1,6 +1,5 @@
-{ config, pkgs, lib, ... }: {
-  # FIXME(2023-02-28): awscli is broken on darwin
-  # home.packages = lib.mkIf pkgs.stdenv.isLinux (with pkgs; [ awscli2 ]);
+{ config, pkgs, ... }: {
+  home.packages = [ pkgs.awscli2 ];
 
   # aws-cli does not work well with xdg base directories
   # https://github.com/aws/aws-sdk/issues/30#issuecomment-532208981
