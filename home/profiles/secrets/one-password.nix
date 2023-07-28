c3248363f8f5a6f5e577bbf412a7b02884d1cc52
Connor Feeley <git@cfeeley.org>
2023-07-27 22:18:42 -0400

    fix: remove one-password HM profile

--- home/profiles/secrets/one-password.nix
+++
@@ -1,7 +0,0 @@
-{ lib, pkgs, ... }:
-lib.mkMerge [
-  { home.packages = [ pkgs._1password ]; }
-  (lib.mkIf (!pkgs.stdenv.hostPlatform.isDarwin) {
-    home.packages = [ pkgs._1password-gui ];
-  })
-]
