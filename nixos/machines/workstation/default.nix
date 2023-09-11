db8ef77b9fface92468ebff60ea89fe9ee0623a2
Connor Feeley <git@cfeeley.org>
2023-09-11 14:15:51 -0400

    wkstn: configure postgresql

--- nixos/machines/workstation/default.nix
+++ nixos/machines/workstation/default.nix
@@ -424,6 +424,31 @@ in
   services.postgresql = rec {
     enable = true;
     package = pkgs.postgresql_14;
-    extraPlugins = with package; [ postgis pg_repack ];
+    extraPlugins = with package.pkgs; [ postgis pg_repack ];
+
+    ensureUsers = [{
+      name = "cfeeley";
+      ensurePermissions = {
+        "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+      };
+      ensureClauses = {
+        superuser = true;
+        createrole = true;
+        createdb = true;
+      };
+    }];
+
+    enableTCPIP = true;
+    authentication = lib.mkOverride 10 ''
+      # type database DBuser auth-method
+      local  all      all    trust
+
+      # type database DBuser origin-address auth-method
+      # IPv4
+      host   all      all     127.0.0.1/32   trust
+      # IPv6
+      host all        all     ::1/128        trust
+
+    '';
   };
 }
