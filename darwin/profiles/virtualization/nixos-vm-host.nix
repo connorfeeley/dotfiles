20317ea3b12c4ce1b46e34cf1b746242687fafe3
Connor Feeley <git@cfeeley.org>
2023-12-01 13:24:53 -0500

    darwin: fix builder

--- darwin/profiles/virtualization/nixos-vm-host.nix
+++ darwin/profiles/virtualization/nixos-vm-host.nix
@@ -1,8 +1,8 @@
 { lib, pkgs, ... }: {
-  environment.systemPackages = [ pkgs.darwin.builder ];
+  environment.systemPackages = [ pkgs.darwin.linux-builder ];
   nix.settings = {
     builders = lib.mkForce [
-      "ssh://builder@localhost aarch64-linux /etc/nix/builder_ed25519 4 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo='"
+      "ssh-ng://builder@linux-builder aarch64-linux /etc/nix/builder_ed25519 8 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo="
     ];
     builders-use-substitutes = true;
   };
