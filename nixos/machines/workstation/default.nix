16f7e35c20908167c1431c6647356cf908fb8271
Connor Feeley <git@cfeeley.org>
2023-12-31 17:13:09 -0500

    wkstn: disable mellanox interface autostart

--- nixos/machines/workstation/default.nix
+++ nixos/machines/workstation/default.nix
@@ -109,19 +109,19 @@ in
       };
 
       # Mellanox 100GbE port 0
-      interfaces.enp36s0f0np0 = {
-        ipv4.addresses = [{
-          address = "192.168.21.50";
-          prefixLength = 24;
-        }];
-      };
+      # interfaces.enp36s0f0np0 = {
+      #   ipv4.addresses = [{
+      #     address = "192.168.21.50";
+      #     prefixLength = 24;
+      #   }];
+      # };
       # Mellanox 100GbE port 1
-      interfaces.enp36s0f0np1 = {
-        ipv4.addresses = [{
-          address = "192.168.20.50";
-          prefixLength = 24;
-        }];
-      };
+      # interfaces.enp36s0f0np1 = {
+      #   ipv4.addresses = [{
+      #     address = "192.168.20.50";
+      #     prefixLength = 24;
+      #   }];
+      # };
     }
   );
 
