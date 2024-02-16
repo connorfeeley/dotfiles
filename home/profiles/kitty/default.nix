5169020bd914a2e9169aeec00b2ca04f76420967
Connor Feeley <git@cfeeley.org>
2024-02-15 21:37:11 -0500

    kitty: add kssh alias and clean up lib.mkMerge

--- home/profiles/kitty/default.nix
+++ home/profiles/kitty/default.nix
@@ -47,68 +47,49 @@ let
         # "+ss12" # Assorted iconizations.
         # "+ss13" # Smooth graphs e.g. for git log.
       ];
-    in
-    ''
-      ${mkFontFeatures' "PragmataProMono" fontStyles fontFeatures}
-    '';
-  # FIXME: reduce the amount of merging -> reduce complecity
+    in "${mkFontFeatures' "PragmataProMono" fontStyles fontFeatures}";
 in
-lib.mkMerge [
-  (lib.mkIf isDarwin {
-  })
+{
+  home.sessionVariables = {
+    KITTY_CONFIG_DIRECTORY = "${config.xdg.configHome}/kitty";
+    # FIXME: necessary?
+    KITTY_SOCKET = socket;
+  };
 
-  {
-    home.sessionVariables = {
-      KITTY_CONFIG_DIRECTORY = "${config.xdg.configHome}/kitty";
-      # FIXME: necessary?
-      KITTY_SOCKET = socket;
+  programs.kitty = {
+    enable = true;
+    settings = settings // {
+      font_size = if (isDarwin && isAarch64) then "12" else "16";
+      confirm_os_window_close = "0";
+      # if (isDarwin)
+      # then "1"
+      # else "0";
+      # 85% opacity
+      background_opacity = if isDarwin then "0.85" else "1.0";
+      dynamic_background_opacity = true;
+
+      kitty_mod = lib.mkIf isLinux "ctrl+shift";
     };
+    # // (lib.mkForce modus-vivendi-faint); # force kitty colorscheme (also set by stylix)
+    keybindings = {
+      # kitty_mod: ctrl+shift, or ⌘ (cmd) key on macos
+      "kitty_mod+n" = "new_os_window_with_cwd";
+      "ctrl+alt+enter" = "launch --cwd=current --location=neighbor";
+      # Default: create new split
+      # "kitty_mod+enter" = "new_window_with_cwd";
 
-    programs.kitty = {
-      enable = true;
-      settings = settings // {
-        font_size = if (isDarwin && isAarch64) then "12" else "16";
-        confirm_os_window_close = "0";
-        # if (isDarwin)
-        # then "1"
-        # else "0";
-        # 85% opacity
-        background_opacity = if isDarwin then "0.85" else "1.0";
-        dynamic_background_opacity = true;
+      # Vim-style window navigation
+      "kitty_mod+left" = "neighboring_window left";
+      "kitty_mod+right" = "neighboring_window right";
+      "kitty_mod+down" = "neighboring_window down";
+      "kitty_mod+up" = "neighboring_window up";
 
-        kitty_mod = lib.mkIf isLinux "ctrl+shift";
-      };
-      # // (lib.mkForce modus-vivendi-faint); # force kitty colorscheme (also set by stylix)
-      keybindings = {
-        # kitty_mod: ctrl+shift, or ⌘ (cmd) key on macos
-        "kitty_mod+n" = "new_os_window_with_cwd";
-        "ctrl+alt+enter" = "launch --cwd=current --location=neighbor";
-        # Default: create new split
-        # "kitty_mod+enter" = "new_window_with_cwd";
+      # Change layouts
+      "kitty_mod+l" = "next_layout";
 
-        # Vim-style window navigation
-        "kitty_mod+left" = "neighboring_window left";
-        "kitty_mod+right" = "neighboring_window right";
-        "kitty_mod+down" = "neighboring_window down";
-        "kitty_mod+up" = "neighboring_window up";
-
-        # Change layouts
-        "kitty_mod+l" = "next_layout";
-      };
-      font = {
-        name = lib.mkForce "Iosevka Term";
-        size = 10;
-      };
-      extraConfig = ''
-        ${lib.optionalString hasPragPro pragmataProExtras}
-
-        # Include theme - symlink to either the selected dark or light theme
-        include ${config.xdg.configHome}/kitty/current-theme.conf
-      '';
-    };
-  }
-  (lib.mkIf isDarwin {
-    programs.kitty.keybindings = {
+      # Open new tab in current directory (and remote host, if using 'kitten ssh HOST')
+      "kitty_mod+t" = "new_window_with_cwd";
+    } // lib.mkIf isDarwin {
       # Tried unbinding ⌘+w from close tab, but it doesn't seem to be taking.
       # Enabled confirm_os_window_close on darwin as a stop-gap.
       "ctrl+shift+w" = "no_op";
@@ -116,38 +97,47 @@ lib.mkMerge [
       "cmd+c" = "copy_to_clipboard";
       "cmd+v" = "paste_from_clipboard";
     };
+    font = {
+      name = lib.mkForce "Iosevka Term";
+      size = 10;
+    };
+    extraConfig = ''
+      ${lib.optionalString hasPragPro pragmataProExtras}
 
-    # Handled by the Homebrew module
-    # This populates a dummy package to satisfy the requirement
-    # programs.kitty.package = pkgs.runCommand "kitty-0.0.0" { } "mkdir $out";
+      # Include theme - symlink to either the selected dark or light theme
+      include ${config.xdg.configHome}/kitty/current-theme.conf
+    '';
+    darwinLaunchOptions = lib.mkIf isDarwin [ "--single-instance" "--listen-on=${socket}" ];
+  };
 
-    programs.kitty.darwinLaunchOptions =
-      [ "--single-instance" "--listen-on=${socket}" ];
+  # Alias kssh -> "kitty ssh"
+  programs.zsh.shellAliases.kssh = "kitten ssh";
 
-  })
-  {
-    xdg.configFile =
-      let
-        chosenTheme = dark;
-        dark = mkThemeBuiltin "Doom_One";
-        light = mkThemeBuiltin "Doom_One_Light";
-      in
-      {
-        ###
-        ### Theming
-        ###
-        "kitty/base16-kitty".source = base16-kitty.outPath;
-        "kitty/nix-kitty-themes".source = pkgs.kitty-themes.outPath;
-        "kitty/themes/dark.conf".source = dark;
-        "kitty/themes/light.conf".source = light;
-        "kitty/current-theme.conf".source = chosenTheme;
+  # Handled by the Homebrew module
+  # This populates a dummy package to satisfy the requirement
+  # programs.kitty.package = lib.mkIf isDarwin (pkgs.runCommand "kitty-0.0.0" { } "mkdir $out");
 
-        "kitty/session".text = ''
-          # Start new sessions in the previous working directory
-          # https://sw.kovidgoyal.net/kitty/overview/#startup-sessions
-          # https://sw.kovidgoyal.net/kitty/faq/#how-do-i-open-a-new-window-or-tab-with-the-same-working-directory-as-the-current-window
-          launch --cwd=current
-        '';
-      };
-  }
-]
+  xdg.configFile =
+    let
+      chosenTheme = dark;
+      dark = mkThemeBuiltin "Doom_One";
+      light = mkThemeBuiltin "Doom_One_Light";
+    in
+    {
+      ###
+      ### Theming
+      ###
+      "kitty/base16-kitty".source = base16-kitty.outPath;
+      "kitty/nix-kitty-themes".source = pkgs.kitty-themes.outPath;
+      "kitty/themes/dark.conf".source = dark;
+      "kitty/themes/light.conf".source = light;
+      "kitty/current-theme.conf".source = chosenTheme;
+
+      "kitty/session".text = ''
+        # Start new sessions in the previous working directory
+        # https://sw.kovidgoyal.net/kitty/overview/#startup-sessions
+        # https://sw.kovidgoyal.net/kitty/faq/#how-do-i-open-a-new-window-or-tab-with-the-same-working-directory-as-the-current-window
+        launch --cwd=current
+      '';
+    };
+}
