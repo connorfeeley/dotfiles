0969981cbe0078b70eef03a456ebd8dbd0a018e3
Connor Feeley <git@cfeeley.org>
2024-02-15 21:37:34 -0500

    fonts: fix on darwin

--- profiles/fonts/common.nix
+++ profiles/fonts/common.nix
@@ -3,50 +3,97 @@ let inherit (pkgs.stdenv.hostPlatform) isLinux isMacOS;
 in {
   environment.systemPackages = lib.optionals isLinux [ pkgs.font-manager ];
 
-  fonts = {
-    fontDir.enable = true;
-    packages = with pkgs;
-      [
-        b612
-        barlow
-        emacs-all-the-icons-fonts
-        # symbola
-        fira
-        ibm-plex
-        inter
-        jost
-        public-sans
-        julia-mono
-        merriweather
-        alegreya
+  fonts =
+    if isLinux then {
+      fontDir.enable = true;
+      packages = with pkgs;
+        [
+          b612
+          barlow
+          emacs-all-the-icons-fonts
+          # symbola
+          fira
+          ibm-plex
+          inter
+          jost
+          public-sans
+          julia-mono
+          merriweather
+          alegreya
 
-        # noto-fonts
-        # noto-fonts-cjk
-        # noto-fonts-emoji
+          # noto-fonts
+          # noto-fonts-cjk
+          # noto-fonts-emoji
 
-        # inputs'.ttc-subway-font.packages.ttc-subway
-        # inputs'.ttc-subway-font.packages.bloor-yonge-font
+          # inputs'.ttc-subway-font.packages.ttc-subway
+          # inputs'.ttc-subway-font.packages.bloor-yonge-font
 
-        (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+          (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
 
-        font-awesome
+          font-awesome
 
-        fira-code
-        fira-code-symbols
-        open-sans
-        jetbrains-mono
-        siji
+          fira-code
+          fira-code-symbols
+          open-sans
+          jetbrains-mono
+          siji
 
-        iosevka-bin
-      ] ++ (lib.optionals isLinux [
-        corefonts
-        inconsolata
-        liberation_ttf
-        dejavu_fonts
-        bakoma_ttf
-        gentium
-        ubuntu_font_family
-        terminus_font
-      ]) ++ (lib.optionals isMacOS [ ]);
-  };
+          iosevka-bin
+        ] ++ (lib.optionals isLinux [
+          corefonts
+          inconsolata
+          liberation_ttf
+          dejavu_fonts
+          bakoma_ttf
+          gentium
+          ubuntu_font_family
+          terminus_font
+        ]) ++ (lib.optionals isMacOS [ ]);
+    }
+    else {
+      fontDir.enable = true;
+      fonts = with pkgs;
+        [
+          b612
+          barlow
+          emacs-all-the-icons-fonts
+          # symbola
+          fira
+          ibm-plex
+          inter
+          jost
+          public-sans
+          julia-mono
+          merriweather
+          alegreya
+
+          # noto-fonts
+          # noto-fonts-cjk
+          # noto-fonts-emoji
+
+          # inputs'.ttc-subway-font.packages.ttc-subway
+          # inputs'.ttc-subway-font.packages.bloor-yonge-font
+
+          (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+
+          font-awesome
+
+          fira-code
+          fira-code-symbols
+          open-sans
+          jetbrains-mono
+          siji
+
+          iosevka-bin
+        ] ++ (lib.optionals isLinux [
+          corefonts
+          inconsolata
+          liberation_ttf
+          dejavu_fonts
+          bakoma_ttf
+          gentium
+          ubuntu_font_family
+          terminus_font
+        ]) ++ (lib.optionals isMacOS [ ]);
+    };
 }
