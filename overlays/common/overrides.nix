c01e7335db7a46d16466648fb4b35957442b744d
Connor Feeley <git@cfeeley.org>
2024-03-23 15:57:00 -0400

    overlay: remove manual nixpkgsDoc overlay

--- overlays/common/overrides.nix
+++ overlays/common/overrides.nix
@@ -15,28 +15,6 @@ channels: final: prev: {
 
   inherit (prev) installApplication;
 
-  # Genarate info pages for nixpkgs
-  # Source: github:aakropotkin/nixpkgs-doc
-  nixpkgsDoc = prev.htmlDocs.nixpkgsManual.overrideAttrs (oldAttrs: {
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ prev.texinfo ];
-    postBuild = ''
-      mkdir texi info
-      sed 's/\(<title><function>\)lib\.[^.]*\.\?\([^.<][^<]\+<\/function><\/title>\)/\1\2/'  \
-        manual-full.xml > manual-full-nodots.xml
-      pandoc -t texinfo -f docbook -o texi/nixpkgs.texi  \
-             manual-full-nodots.xml
-      makeinfo --force -o info texi/nixpkgs.texi
-    '';
-    installPhase = ''
-      runHook preInstall
-      ${oldAttrs.installPhase}
-      runHook postInstall
-    '';
-    postInstall = ''
-      cp -pr --reflink=auto -- info "$out/share/"
-    '';
-  });
-
   # Fix nixos-option flake support
   nixos-option = prev.symlinkJoin {
     name = "nixos-option";
