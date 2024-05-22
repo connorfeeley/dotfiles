a6e866e77804280261aaaff5ceee1e9c48bca68f
Connor Feeley <git@cfeeley.org>
2024-05-22 09:00:00 -0400

    darwin(pulseaudio): enable brew service

--- darwin/profiles/pulseaudio.nix
+++ darwin/profiles/pulseaudio.nix
@@ -1,1 +1,13 @@
-{ config, lib, pkgs, ... }: { homebrew.brews = [{ name = "pulseaudio"; }]; }
+# SPDX-FileCopyrightText: 2024 Connor Feeley
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+{ ... }: {
+  homebrew.brews = [
+    {
+      name = "pulseaudio";
+      start_service = true;
+      restart_service = "changed";
+    }
+  ];
+}
