dce665105e009f5a1e21342ccbb6d82185b20bce
Connor Feeley <git@cfeeley.org>
2024-05-30 09:00:00 -0400

    home(work): HM remmina module not available yet

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -8,14 +8,16 @@ in {
     self'.packages.pwrbar # control Kasa WiFi powerbar
     pkgs.python3Packages.python-kasa
   ] ++ lib.optionals isLinux [
+    pkgs.remmina  # FIXME(2024-05-30): HM module not available on 23.11. Waiting for 24.05.
     pkgs.freerdp
     pkgs.gparted
   ];
 
   # RDP/VNC client
-  services.remmina = lib.mkIf isLinux {
-    enable = true;
-    addRdpMimeTypeAssoc = true;
-    systemdService.enable = true;
-  };
+  # FIXME(2024-05-30): HM module not available on 23.11. Waiting for 24.05.
+  # services.remmina = lib.mkIf isLinux {
+  #   enable = true;
+  #   addRdpMimeTypeAssoc = true;
+  #   systemdService.enable = true;
+  # };
 }
