d20cbcab793d6720bb054fd88102743af2b8904d
Connor Feeley <git@cfeeley.org>
2024-06-04 09:00:00 -0400

    nixos(amd): use ryzen-smu config option

--- nixos/profiles/hardware/amd.nix
+++ nixos/profiles/hardware/amd.nix
@@ -2,15 +2,10 @@
 lib.mkIf (!config.nixos-vm.enable) {
   boot.kernelModules = [ "kvm-amd" ];
   boot.initrd.kernelModules = [ "kvm-amd" ];
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  # Driver that exposes access to the SMU (System Management Unit) for certain AMD Ryzen Processors
-  boot.extraModulePackages = [
-    (pkgs.nur.repos.arc.packages.ryzen-smu.override {
-      linux = config.boot.kernelPackages.kernel;
-    })
-  ];
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   programs.ryzen-monitor-ng.enable = true;
+
+  # Driver that exposes access to the SMU (System Management Unit) for certain AMD Ryzen Processors
+  hardware.cpu.amd.ryzen-smu.enable = true;
 }
