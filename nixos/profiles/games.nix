d14aaeb14bcbbe448926d0398d79132c0965e0b0
Connor Feeley <git@cfeeley.org>
2024-06-04 09:00:00 -0400

    nixos(games): minecraft launcher broken

--- nixos/profiles/games.nix
+++ nixos/profiles/games.nix
@@ -3,7 +3,8 @@ lib.mkIf (!config.nixos-vm.enable) {
   environment.systemPackages = with pkgs; [
     discord
 
-    minecraft
+    # FIXME(2024-06-04): broken
+    # minecraft
     ferium # CLI program for managing Minecraft modpacks from Modrinth, CurseForge, and Github Releases
 
     ckan # Mod manager for Kerbal Space Program
