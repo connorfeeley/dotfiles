ecbb1764c2a4cd739f368c5f5c11c2350cd80425
Connor Feeley <git@cfeeley.org>
2024-06-05 09:00:00 -0400

    nixos(sddm): enable wayland

--- nixos/profiles/login/sddm.nix
+++ nixos/profiles/login/sddm.nix
@@ -4,13 +4,24 @@
 
 { ... }:
 {
-  security.pam.services.sddm.enableGnomeKeyring = true;
+  security.pam.services = {
+    login.gnupg = {
+      enable = true;
+      noAutostart = true;
+    };
+
+    sddm.gnupg = {
+      enable = true;
+      noAutostart = true;
+    };
+  };
 
   services.xserver = {
     displayManager.sddm = {
       enable = true;
       enableHidpi = true;
       autoNumlock = true;
+      wayland.enable = true;
       # settings = { Autologin = { Session = "plasma.desktop"; User = "john"; } ; };
     };
   };
