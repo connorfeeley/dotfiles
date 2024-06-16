5393ba01b1d2d57b65a27c580c1fc518a0253a99
Connor Feeley <git@cfeeley.org>
2024-06-16 09:00:00 -0400

    flake(overrides): use webtorrent-cli from nixos-stable

--- home/profiles/misc.nix
+++ home/profiles/misc.nix
@@ -1,50 +1,49 @@
 { inputs', self', lib, pkgs, ... }: {
   # FIXME: most of these should NOT be considered part of "core"
   # most don't belong on a server, etc.
-  home.packages = with pkgs;
-    [
-      ## === Uncategorized Packages ===
-      just # <- Just a command runner
-      speedtest-cli
-      asciiquarium
-      nodePackages.webtorrent-cli
-      inputs'.nix-search-cli.packages.nix-search
-      dogdns
+  home.packages = with pkgs; [
+    ## === Uncategorized Packages ===
+    just # <- Just a command runner
+    speedtest-cli
+    asciiquarium
+    webtorrent-cli
+    inputs'.nix-search-cli.packages.nix-search
+    dogdns
 
-      ## === Data and Documents ===
-      tidy-viewer # <- Pretty-print CSV files
-      xsv # <- A fast CSV command line toolkit
-      python3Packages.yq # <- jq wrapper for YAML/XML/TOML documents
-      htmlq # <- Like jq, but for HTML
-      # FIXME(2024-05-26): broken
-      # csvkit # <- CSV file processing utilities
-      pdftk # <- PDF toolkit
-      inputs'.nurpkgs.packages.pdftocgen # <- PDF table of contents generator
+    ## === Data and Documents ===
+    tidy-viewer # <- Pretty-print CSV files
+    xsv # <- A fast CSV command line toolkit
+    python3Packages.yq # <- jq wrapper for YAML/XML/TOML documents
+    htmlq # <- Like jq, but for HTML
+    # FIXME(2024-05-26): broken
+    # csvkit # <- CSV file processing utilities
+    pdftk # <- PDF toolkit
+    inputs'.nurpkgs.packages.pdftocgen # <- PDF table of contents generator
 
-      ## === Files ===
-      file
-      mediainfo
-      zip
-      unzip
-      pigz
+    ## === Files ===
+    file
+    mediainfo
+    zip
+    unzip
+    pigz
 
-      ## === Data Sync ===
-      rclone
-      inetutils # <- ftp, rcp, etc.
+    ## === Data Sync ===
+    rclone
+    inetutils # <- ftp, rcp, etc.
 
-      ## === Repo Scripts ===
-      # (lib.hiPrio n)
-      # (lib.hiPrio nb)
-      # nixos-rebuild-remote
-      # self'.packages.nomos-rebuild
-      # dotfield-sync
-      # dotfield-push
-      # dotfield-rebuild
-      # dotfield-doom
-    ] ++ (lib.optionals (pkgs.stdenv.isLinux) [
-      handbrake # <- Video transcoder
-      mmdoc # <- Markdown documentation generator
-    ]);
+    ## === Repo Scripts ===
+    # (lib.hiPrio n)
+    # (lib.hiPrio nb)
+    # nixos-rebuild-remote
+    # self'.packages.nomos-rebuild
+    # dotfield-sync
+    # dotfield-push
+    # dotfield-rebuild
+    # dotfield-doom
+  ] ++ (lib.optionals (pkgs.stdenv.isLinux) [
+    handbrake # <- Video transcoder
+    mmdoc # <- Markdown documentation generator
+  ]);
 
   fonts.fontconfig.enable = true;
 
