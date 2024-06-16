1c4800026917dfe2310251455e3690d8dd9394cb
Connor Feeley <git@cfeeley.org>
2024-06-16 09:00:00 -0400

    flake: just comment ttc-subway-font input

--- flake.nix
+++ flake.nix
@@ -127,6 +127,7 @@
     ##: --- personal packages --------------------------------------------------
     nurpkgs = { url = "github:connorfeeley/nurpkgs"; inputs.nixpkgs.follows = "nixpkgs"; };
     xmonad-config = { url = "git+https://git.sr.ht/~cfeeley/xmonad-config"; };
+    # ttc-subway-font = { url = "git+ssh://git@git.sr.ht/~cfeeley/ttc-subway-font"; inputs.nixpkgs.follows = "nixpkgs"; }; # Private repo
 
     ##: --- meta packages ------------------------------------------------------
     emacs-overlay = { url = "github:nix-community/emacs-overlay"; inputs.nixpkgs.follows = "nixpkgs"; };
