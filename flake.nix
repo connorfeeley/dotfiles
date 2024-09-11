92cbbea3f1a8fd2e1fd994c951dc6434be395d7d
Connor Feeley <git@cfeeley.org>
2024-09-11 09:00:00 -0400

    flake: track correct HM

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
     nixos-23-11.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     ##: --- system -------------------------------------------------------------
-    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    home-manager = { url = "github:nix-community/home-manager/release-24.05"; inputs.nixpkgs.follows = "nixpkgs"; };
     darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
     digga = { url = "github:divnix/digga"; inputs.nixpkgs.follows = "nixpkgs"; inputs.home-manager.follows = "home-manager"; inputs.darwin.follows = "darwin"; };
     nixos-wsl = { url = "github:nix-community/NixOS-WSL"; inputs.nixpkgs.follows = "nixpkgs"; };
