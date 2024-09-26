6aba394acb69a36b521b510220c082bbfd852ca8
Connor Feeley <git@cfeeley.org>
2024-09-26 09:00:00 -0400

    nixos(wifi): try fixing wait-online

--- nixos/profiles/networking/wifi.nix
+++ nixos/profiles/networking/wifi.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Prevent stupid boot delays waiting for internet.
   # https://discourse.nixos.org/t/boot-faster-by-disabling-udev-settle-and-nm-wait-online/6339
   systemd.services = {
     systemd-udev-settle.enable = false;
-    NetworkManager-wait-online.enable = false;
+    NetworkManager-wait-online = {
+      serviceConfig = {
+        ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q" ];
+      };
+    };
   };
 }
