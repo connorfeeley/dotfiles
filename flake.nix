cefa648f71be512353c0b0355bf48a1cf632b79c
Connor Feeley <git@cfeeley.org>
2025-03-06 09:00:00 -0500

    nix: track nix-darwin branch

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
-      "https://workstation.elephant-vibes.ts.net/cfeeley"
+      # "https://workstation.elephant-vibes.ts.net/cfeeley"
       "https://cfeeley.cachix.org"
     ];
     extra-trusted-public-keys = [
@@ -79,8 +79,8 @@
     nixpkgs.follows = "nixos-stable-darwin";
     nixpkgs-darwin.follows = "nixos-unstable";
 
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixos-stable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixos-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-stable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small"; # For docker_24
 
@@ -89,7 +89,7 @@
 
     ##: --- system -------------------------------------------------------------
     home-manager = { url = "github:nix-community/home-manager/release-24.05"; inputs.nixpkgs.follows = "nixpkgs"; };
-    darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
+    darwin = { url = "github:LnL7/nix-darwin/nix-darwin-24.11"; inputs.nixpkgs.follows = "nixpkgs"; };
     digga = { url = "github:divnix/digga"; inputs.nixpkgs.follows = "nixpkgs"; inputs.home-manager.follows = "home-manager"; inputs.darwin.follows = "darwin"; };
     nixos-wsl = { url = "github:nix-community/NixOS-WSL"; inputs.nixpkgs.follows = "nixpkgs"; };
     agenix = { url = "github:ryantm/agenix"; inputs.nixpkgs.follows = "nixpkgs"; };
